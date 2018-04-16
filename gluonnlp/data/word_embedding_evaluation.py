d17f5735a6ea76f8960652549484def9e814ba96
Leonard Lausen <leonard@lausen.nl>
2018-04-16 20:25:48 +0000

    Fix SSL certificate checking for misbehaving servers

--- gluonnlp/data/word_embedding_evaluation.py
+++ gluonnlp/data/word_embedding_evaluation.py
@@ -23,23 +23,87 @@
 import os
 import tarfile
 import zipfile
+try:
+    import requests
+except ImportError:
+    class requests_failed_to_import(object):
+        pass
+    requests = requests_failed_to_import
 
 import pandas as pd
 from mxnet.gluon.data.dataset import Dataset
-from mxnet.gluon.utils import check_sha1, download
+from mxnet.gluon.utils import check_sha1
 
 word_similarity_datasets = [
     'WordSim353', 'MEN', 'RadinskyMTurk', 'RareWords', 'SimLex999',
-    'SimVerb3500', 'SemEval17Task2', 'BakerVerb143', 'YangPowersVerb130',
-    'GoogleAnalogyTestSet', 'BiggerAnalogyTestSet'
+    'SimVerb3500', 'SemEval17Task2', 'BakerVerb143', 'YangPowersVerb130'
 ]
+word_analogy_datasets = ['GoogleAnalogyTestSet', 'BiggerAnalogyTestSet']
 __all__ = word_similarity_datasets
+
+
+# TODO Remove once verify support is merged in mxnet.gluon.utils.download
+def download(url, path=None, overwrite=False, sha1_hash=None, verify=True):
+    """Download an given URL
+
+    Parameters
+    ----------
+    url : str
+        URL to download
+    path : str, optional
+        Destination path to store downloaded file. By default stores to the
+        current directory with same name as in url.
+    overwrite : bool, optional
+        Whether to overwrite destination file if already exists.
+    sha1_hash : str, optional
+        Expected sha1 hash in hexadecimal digits. Will ignore existing file when hash is specified
+        but doesn't match.
+    verify : bool
+        Toggle verification of SSL certificates.
+
+    Returns
+    -------
+    str
+        The file path of the downloaded file.
+    """
+    if path is None:
+        fname = url.split('/')[-1]
+    else:
+        path = os.path.expanduser(path)
+        if os.path.isdir(path):
+            fname = os.path.join(path, url.split('/')[-1])
+        else:
+            fname = path
+
+    if overwrite or not os.path.exists(fname) or (
+            sha1_hash and not check_sha1(fname, sha1_hash)):
+        dirname = os.path.dirname(os.path.abspath(os.path.expanduser(fname)))
+        if not os.path.exists(dirname):
+            os.makedirs(dirname)
+
+        print('Downloading %s from %s...' % (fname, url))
+        r = requests.get(url, stream=True, verify=verify)
+        if r.status_code != 200:
+            raise RuntimeError('Failed downloading url %s' % url)
+        with open(fname, 'wb') as f:
+            for chunk in r.iter_content(chunk_size=1024):
+                if chunk:  # filter out keep-alive new chunks
+                    f.write(chunk)
+
+        if sha1_hash and not check_sha1(fname, sha1_hash):
+            raise UserWarning('File {} is downloaded but the content hash does not match. ' \
+                              'The repo may be outdated or download may be incomplete. ' \
+                              'If the "repo_url" is overridden, consider switching to ' \
+                              'the default repo.'.format(fname))
+
+    return fname
 
 
 class _Dataset(Dataset):
     _url = None  # Dataset is retrieved from here if not cached
     _archive_file = None  # Archive name and checksum
     _checksums = None  # Checksum of archive contents
+    _verify_ssl = True  # Verify SSL certificates when downloading from self._url
 
     _data = None  # Internal dataset representation
 
@@ -61,7 +125,10 @@ class _Dataset(Dataset):
             path = os.path.join(self.root, name)
             if not os.path.exists(path) or not check_sha1(path, checksum):
                 downloaded_file_path = download(
-                    self._url, path=self.root, sha1_hash=archive_hash)
+                    self._url,
+                    path=self.root,
+                    sha1_hash=archive_hash,
+                    verify=self._verify_ssl)
 
                 if downloaded_file_path.lower().endswith('zip'):
                     with zipfile.ZipFile(downloaded_file_path, 'r') as zf:
@@ -722,6 +789,7 @@ class BakerVerb143(_WordSimilarityEvaluationDataset):
         'verb_similarity dataset.txt':
         'd7e4820c7504cbae56898353e4d94e6408c330fc'
     }
+    _verify_ssl = False  # ie.technion.ac.il serves an invalid cert as of 2018-04-16
 
     min = 0
     max = 10
