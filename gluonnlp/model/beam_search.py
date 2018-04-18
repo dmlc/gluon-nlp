b820406d1f0d80b77ebf70f423f98befae6d887e
Xingjian Shi <xshiab@ust.hk>
2018-04-18 02:38:12 -0700

    absolute import

--- gluonnlp/model/beam_search.py
+++ gluonnlp/model/beam_search.py
@@ -17,6 +17,9 @@
 # specific language governing permissions and limitations
 # under the License.
 """Implements the beam search sampler."""
+from __future__ import absolute_import
+from __future__ import print_function
+
 __all__ = ['BeamSearchScorer', 'BeamSearchSampler']
 
 import numpy as np
