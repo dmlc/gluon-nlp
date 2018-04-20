a554377732fccf084ed7f935eddfa7cb2ce6711f
Xingjian Shi <xshiab@ust.hk>
2018-04-20 13:15:27 -0700

    fix

--- scripts/nmt/train.py
+++ scripts/nmt/train.py
@@ -29,6 +29,7 @@ def cache_dataset(dataset, prefix):
 def load_cached_dataset(prefix):
     cached_file_path = os.path.join(_C.CACHE_PATH, prefix + '.npz')
     if os.path.exists(cached_file_path):
+        print('Load cached data from {}'.format(cached_file_path))
         dat = np.load(cached_file_path)
         return ArrayDataset(np.array(dat['src_data']), np.array(dat['tgt_data']))
     else:
