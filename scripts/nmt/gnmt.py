c35c8c4cb79ff6603655f13bf2712bebec971597
Xingjian Shi <xshiab@ust.hk>
2018-04-21 18:21:00 -0700

    fix

--- scripts/nmt/gnmt.py
+++ scripts/nmt/gnmt.py
@@ -27,36 +27,36 @@ mx.random.seed(10000)
 parser = argparse.ArgumentParser(description='Neural Machine Translation Example.'
                                              'We train the Google NMT model')
 parser.add_argument('--dataset', type=str, default="IWSLT2015", help='Dataset to use.')
-parser.add_argument('--src-lang', type=str, default="en", help='Source language')
-parser.add_argument('--tgt-lang', type=str, default="vi", help='Target language')
+parser.add_argument('--src_lang', type=str, default="en", help='Source language')
+parser.add_argument('--tgt_lang', type=str, default="vi", help='Target language')
 parser.add_argument('--epochs', type=int, default=20, help='upper epoch limit')
-parser.add_argument('--num-hidden', type=int, default=128, help='Dimension of the embedding '
+parser.add_argument('--num_hidden', type=int, default=128, help='Dimension of the embedding '
                                                                 'vectors and states.')
 parser.add_argument('--dropout', type=float, default=0.2,
                     help='dropout applied to layers (0 = no dropout)')
-parser.add_argument('--num-layers', type=int, default=2, help='number of layers in the encoder'
+parser.add_argument('--num_layers', type=int, default=2, help='number of layers in the encoder'
                                                               ' and decoder')
-parser.add_argument('--num-bi-layers', type=int, default=1,
+parser.add_argument('--num_bi_layers', type=int, default=1,
                     help='number of bidirectional layers in the encoder and decoder')
-parser.add_argument('--batch-size', type=int, default=128, help='Batch size')
-parser.add_argument('--beam-size', type=int, default=1, help='Beam size')
-parser.add_argument('--lp-alpha', type=float, default=0.0, help='Alpha used in calculating the length penalty')
-parser.add_argument('--lp-k', type=int, default=5, help='K used in calculating the length penalty')
-parser.add_argument('--test-batch-size', type=int, default=32, help='Test batch size')
-parser.add_argument('--num-buckets', type=int, default=5, help='Bucket number')
-parser.add_argument('--bucket-ratio', type=float, default=0.0, help='Ratio for increasing the '
+parser.add_argument('--batch_size', type=int, default=128, help='Batch size')
+parser.add_argument('--beam_size', type=int, default=1, help='Beam size')
+parser.add_argument('--lp_alpha', type=float, default=0.0, help='Alpha used in calculating the length penalty')
+parser.add_argument('--lp_k', type=int, default=5, help='K used in calculating the length penalty')
+parser.add_argument('--test_batch_size', type=int, default=32, help='Test batch size')
+parser.add_argument('--num_buckets', type=int, default=5, help='Bucket number')
+parser.add_argument('--bucket_ratio', type=float, default=0.0, help='Ratio for increasing the '
                                                                     'throughput of the bucketing')
-parser.add_argument('--src-max-len', type=int, default=50, help='Maximum length of the source '
+parser.add_argument('--src_max_len', type=int, default=50, help='Maximum length of the source '
                                                                 'sentence')
-parser.add_argument('--tgt-max-len', type=int, default=50, help='Maximum length of the target '
+parser.add_argument('--tgt_max_len', type=int, default=50, help='Maximum length of the target '
                                                                 'sentence')
 parser.add_argument('--optimizer', type=str, default='adam', help='optimization algorithm')
 parser.add_argument('--lr', type=float, default=1E-3, help='Initial learning rate')
-parser.add_argument('--lr-update-factor', type=float, default=0.5, help='Learning rate decay factor')
+parser.add_argument('--lr_update_factor', type=float, default=0.5, help='Learning rate decay factor')
 parser.add_argument('--clip', type=float, default=5.0, help='gradient clipping')
-parser.add_argument('--log-interval', type=int, default=100, metavar='N',
+parser.add_argument('--log_interval', type=int, default=100, metavar='N',
                     help='report interval')
-parser.add_argument('--save-dir', type=str, default='out_dir',
+parser.add_argument('--save_dir', type=str, default='out_dir',
                     help='directory path to save the final model and training log')
 parser.add_argument('--gpu', type=int, default=None,
                     help='id of the gpu to use. Set it to empty means to use cpu.')
