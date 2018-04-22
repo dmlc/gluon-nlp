928c8b51993eeb4284603002580c7605bbd9b18e
Xingjian Shi <xshiab@ust.hk>
2018-04-22 02:14:32 -0700

    try to fix lint

--- gluonnlp/model/encoder_decoder.py
+++ gluonnlp/model/encoder_decoder.py
@@ -1,12 +1,31 @@
+# coding: utf-8
+
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+"""Encoder/decoders usded in sequence-to-sequence learning."""
+__all__ = ['Seq2SeqEncoder', 'Seq2SeqDecoder',
+           'GNMTEncoder', 'GNMTDecoder', 'get_gnmt_encoder_decoder']
+
 import mxnet as mx
 from functools import partial
 from mxnet.base import _as_list
 from mxnet.gluon import nn, rnn
 from mxnet.gluon.block import Block, HybridBlock
 from .attention_cell import *
-
-__all__ = ['Seq2SeqEncoder', 'Seq2SeqDecoder',
-           'GNMTEncoder', 'GNMTDecoder', 'get_gnmt_encoder_decoder']
 
 
 def _list_bcast_where(F, mask, new_val_l, old_val_l):
