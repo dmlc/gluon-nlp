4cacc7c366e554975884d23085d79b8594f46275
Sheng Zha <szha@users.noreply.github.com>
2018-04-27 12:54:24 -0700

    Fix setup.py on windows with non-UTF8 default encoding

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
-import os
 import io
+import os
 import re
 import shutil
 import sys
@@ -24,7 +24,7 @@ def find_version(*file_paths):
     raise RuntimeError("Unable to find version string.")
 
 
-readme = open('README.rst').read()
+readme = io.open('README.rst', encoding='utf-8').read()
 
 VERSION = find_version('gluonnlp', '__init__.py')
 
