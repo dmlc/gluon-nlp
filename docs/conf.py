c3aec947c027ad862cd5a083008398eab6ce6126
Leonard Lausen <leonard@lausen.nl>
2018-04-28 00:53:46 +0000

    Add .ipynb_checkpoints to sphinx exclude_patterns
    This is required: https://media.readthedocs.org/pdf/nbsphinx/latest/nbsphinx.pdf
    Otherwise we will get a lot of warnings if the environment contains
    ipynb_checkpoints..

--- docs/conf.py
+++ docs/conf.py
@@ -111,7 +111,7 @@ html_favicon = '_static/gluon_s2.png'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
-exclude_patterns = ['_build']
+exclude_patterns = ['_build', '**.ipynb_checkpoints']
 
 # The reST default role (used for this markup: `text`) to use for all
 # documents.
