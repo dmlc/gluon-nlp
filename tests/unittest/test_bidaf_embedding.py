cf5d358fc33aeda6bf1727d162f8af4d2c344801
Sergey Sokolov <sssokolo@amazon.com>
2018-05-24 11:28:30 -0700

    Add support for Python 2.x

--- tests/unittest/test_bidaf_embedding.py
+++ tests/unittest/test_bidaf_embedding.py
@@ -8,20 +8,17 @@ from gluonnlp.model.block import CharacterLevelCNNEmbedding, PredefinedEmbedding
 
 
 def test_create_char_embedding():
-    input = 'my custom string'
-    vocab = nlp.Vocab(nlp.data.count_tokens(input.__iter__(), to_lower=True))
-
-    char_vocab_size = len(vocab)
-    char_emb_size = 8
+    input_text = 'my custom string'
+    vocab = nlp.Vocab(nlp.data.count_tokens(list(iter(input_text)), to_lower=True))
 
     net = nn.HybridSequential()
     with net.name_scope():
         net.add(CharacterLevelCNNEmbedding(channels=[100], kernel_sizes=[5], padding=-1,
-                                           vocab_size=char_vocab_size))
+                                           vocab_size=len(vocab)))
 
     net.initialize(init.Xavier(magnitude=2.24))
 
-    indices = nd.array([vocab(list(input.__iter__())[0:10])])
+    indices = nd.array([vocab(list(iter(input_text))[0:10])])
     print(indices)
     out = net.forward(indices)
     print(out)
@@ -29,14 +26,14 @@ def test_create_char_embedding():
 
 def test_create_word_embedding_layer():
     glove_embedding = nlp.embedding.create('glove', source='glove.6B.100d')
-    input = 'my custom string'
+    input_text = 'my custom string'
 
     net = nn.HybridSequential()
     with net.name_scope():
         net.add(PredefinedEmbedding(glove_embedding))
 
     net.initialize(init.Xavier(magnitude=2.24))
-    tokens = _simple_tokenize(input)
+    tokens = _simple_tokenize(input_text)
     counter = nlp.data.count_tokens(tokens)
     vocab = nlp.Vocab(counter)
     vocab.set_embedding(glove_embedding)
