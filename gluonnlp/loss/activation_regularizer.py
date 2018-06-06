65aff340a299d491aa43abc6fbb5ee6bf8aff9e1
Chenguang Wang <chgwang@amazon.com>
2018-06-06 12:01:31 -0700

    Fix loss docs

--- gluonnlp/loss/activation_regularizer.py
+++ gluonnlp/loss/activation_regularizer.py
@@ -62,8 +62,9 @@ class ActivationRegularizationLoss(Loss):
     states :
         the stack outputs from RNN, which consists of output from each time step (TNC).
 
+
     Outputs
-    -------
+    --------
     loss :
         loss tensor with shape (batch_size,). Dimensions other than batch_axis are averaged out.
     """
