cabc31683f3a526b53c671cd0b12768b800ece44
eric-haibin-lin <linhaibin.eric@gmail.com>
2018-06-06 18:15:17 +0000

    sample block

--- gluonnlp/model/block.py
+++ gluonnlp/model/block.py
@@ -22,6 +22,7 @@ __all__ = ['RNNCellLayer', 'L2Normalization']
 
 from mxnet import ndarray
 from mxnet.gluon import Block, HybridBlock
+#from mxnet.gluon.contrib import SparseBlock
 
 
 class RNNCellLayer(Block):
@@ -76,3 +77,87 @@ class L2Normalization(HybridBlock):
     def hybrid_forward(self, F, x):  # pylint: disable=arguments-differ
         ret = F.broadcast_div(x, F.norm(x, axis=self._axis, keepdims=True) + self._eps)
         return ret
+
+class SampledLogits(SparseBlock):
+    """Block that passes through the input directly.
+
+    This block can be used in conjunction with HybridConcurrent
+    block for residual connection.
+
+            This under-estimates the full softmax and is only used for training.
+
+    Example::
+
+        inputs = ...
+        # for testing
+        test = nn.Dense(inputs, ...)
+        # for training
+        train = contrib.nn.SampledLogits(inputs, params=test.params)
+
+    Parameters
+    ----------
+    num_classes: int
+        Number of possible classes.
+    num_sampled: int
+        Number of classes randomly sampled for each batch.
+    in_unit: int
+        Dimensionality of the input space.
+    remove_accidental_hits: bool
+        Whether to remove "accidental hits" when a sampled candidate is equal to
+        one of the true classes.
+    """
+    def __init__(self, num_classes, num_sampled, in_unit, remove_accidental_hits,
+                 prefix=None, params=None):
+        super(SampledLogits, self).__init__(prefix=prefix, params=params)
+        #self.weight = 
+        self.num_classes = num_classes
+        self.num_sampled = num_sampled
+        self.in_unit = in_unit
+        self.remove_accidental_hits = remove_accidental_hits
+
+    def sparse_forward(self, x, sampled_candidates, expected_count_sampled,
+                       expected_count_true, label, weight, bias):
+        F = mx.ndarray
+        # (n,)
+        label = F.reshape(label, shape=(-1,))
+        # (num_sampled+n, )
+        ids_all = F.concat(sampled_candidates, label, dim=0)
+        # lookup weights and biases
+        # (num_sampled+n, dim)
+        w_all = F.Embedding(data=ids_all, weight=weight,
+                            in_unit=self.num_classes, output_dim=self.in_unit,
+                            sparse_grad=True)
+        # (num_sampled+n, 1)
+        b_all = F.Embedding(data=ids_all, weight=bias,
+                            in_unit=self.num_classes, output_dim=1,
+                            sparse_grad=True)
+        # (num_sampled, dim)
+        w_sampled = w_all.slice(begin=(0, 0), end=(self.num_sampled, None))
+        w_true = w_all.slice(begin=(self.num_sampled, 0), end=(None, None))
+        b_sampled = b_all.slice(begin=(0, 0), end=(self.num_sampled, None))
+        b_true = b_all.slice(begin=(self.num_sampled, 0), end=(None, None))
+        # true
+        # (n, 1)
+        logits_true = (w_true * x).sum(axis=1, keepdims=True) + b_true
+        # samples
+        # (n, num_sampled)
+        b_sampled = F.reshape(b_sampled, (-1,))
+        logits_sampled = F.FullyConnected(x, weight=w_sampled, bias=b_sampled,
+                                          num_hidden=self.num_sampled)
+
+        # remove accidental hits
+        if self.remove_accidental_hits:
+            label_vec = F.reshape(label, (-1, 1))
+            sample_vec = F.reshape(sampled_candidates, (1, -1))
+            mask = F.broadcast_equal(label_vec, sample_vec) * -1e37
+            logits_sampled = logits_sampled + mask
+
+        expected_count_sampled = F.reshape(expected_count_sampled,
+                                           shape=(1, self.num_sampled))
+        logits_true = logits_true - F.log(expected_count_true)
+        logits_sampled = F.broadcast_sub(logits_sampled, F.log(expected_count_sampled))
+
+        # return logits and new_labels
+        # (n, 1+num_sampled)
+        logits = F.concat(logits_true, logits_sampled, dim=1)
+        return logits, F.zeros_like(label)
