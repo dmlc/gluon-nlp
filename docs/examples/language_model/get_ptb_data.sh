66edb031180da1d4f0a03a420098057bc46d28ba
Ubuntu <ubuntu@ip-172-31-20-26.us-west-2.compute.internal>
2018-06-08 22:50:46 +0000

    Add cache and awd language model usage and fix the notebook example

--- docs/examples/language_model/get_ptb_data.sh
+++ docs/examples/language_model/get_ptb_data.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 curl -O http://www.fit.vutbr.cz/~imikolov/rnnlm/simple-examples.tgz
-tar --strip-components=3 -xzvf simple-examples.tgz simple-examples/data/ptb.train.txt simple-examples/data/ptb.valid.txt simple-examples/data/ptb.test.txt
+tar --strip-components=3 -xzvf simple-examples.tgz ./simple-examples/data/ptb.train.txt ./simple-examples/data/ptb.valid.txt ./simple-examples/data/ptb.test.txt
