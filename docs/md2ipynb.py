f927d565eba2bb57fbb2fd50062c07ee34708437
Sheng Zha <zhasheng@amazon.com>
2018-07-09 10:49:34 -0700

    fix

--- docs/md2ipynb.py
+++ docs/md2ipynb.py
@@ -30,7 +30,7 @@ if do_eval and not any([i in input_fn for i in ignore_execution]):
 
 # write
 # need to add language info to for syntax highlight
-notebook['metadata'].update({'language_info':{'name':'ipython3'}})
+notebook['metadata'].update({'language_info': {'name': 'ipython'}})
 
 with open(output_fn, 'w') as f:
     f.write(nbformat.writes(notebook))
