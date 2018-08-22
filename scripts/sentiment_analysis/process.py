b2d00ebecd3e0d2d2fa54492f9f72eb7d2a34b33
xiaotinghe <master@hexiaoting.com>
2018-08-22 15:18:40 +0800

    Update process.py

--- scripts/sentiment_analysis/process.py
+++ scripts/sentiment_analysis/process.py
@@ -1,7 +1,8 @@
 import re
 import multiprocessing as mp
-import gluonnlp as nlp
 import time
+
+import gluonnlp as nlp
 from mxnet import nd, gluon
 
 vocab = None
@@ -26,7 +27,7 @@ def _load_file(data_name):
         train_dataset = nlp.data.SUBJ(root='data/Subj', segment='all')
         output_size = 2
         return train_dataset, output_size
-    elif data_name == 'TREC':
+    else data_name == 'TREC':
         train_dataset, test_dataset = [nlp.data.TREC(root='data/trec', segment=segment)
                                        for segment in ('train', 'test')]
         output_size = 6
@@ -34,28 +35,26 @@ def _load_file(data_name):
 
 def _clean_str(string, data_name):
     if data_name == 'SST-1' or data_name == 'SST-2':
-        string = re.sub(r"[^A-Za-z0-9(),!?\'\`]", " ", string) 
-        string = re.sub(r"\s{2,}", " ", string) 
+        string = re.sub(r"[^A-Za-z0-9(),!?\'\`]", " ", string)
+        string = re.sub(r"\s{2,}", " ", string)
         return string.strip().lower()
     else:
-        string = re.sub(r"[^A-Za-z0-9(),!?\'\`]", " ", string)
-        string = re.sub(r"\'s", " 's", string)
-        string = re.sub(r"\'ve", " 've", string)
-        string = re.sub(r"n\'t", " n't", string)
-        string = re.sub(r"\'re", " 're", string)
-        string = re.sub(r"\'d", " 'd", string)
-        string = re.sub(r"\'ll", " 'll", string)
-        string = re.sub(r",", " , ", string)
-        string = re.sub(r"!", " ! ", string)
-        string = re.sub(r"\(", " ( ", string)
-        string = re.sub(r"\)", " ) ", string)
-        string = re.sub(r"\?", " ? ", string)
-        string = re.sub(r"\s{2,}", " ", string)
+        string = re.sub(r'[^A-Za-z0-9(),!?\'\`]', ' ', string)
+        string = re.sub(r'\'s', ' \'s', string)
+        string = re.sub(r'\'ve', ' \'ve', string)
+        string = re.sub(r'n\'t', ' n\'t', string)
+        string = re.sub(r'\'re', ' \'re', string)
+        string = re.sub(r'\'d', ' \'d', string)
+        string = re.sub(r'\'ll', ' \'ll', string)
+        string = re.sub(r',', ' , ', string)
+        string = re.sub(r'!', ' ! ', string)
+        string = re.sub(r'\(', ' ( ', string)
+        string = re.sub(r'\)', ' ) ', string)
+        string = re.sub(r'\?', ' ? ', string)
+        string = re.sub(r'\s{2,}', ' ', string)
         return string.strip() if data_name == 'TREC' else string.strip().lower()
 
-def _build_vocab(data_name, train_dataset, test_dataset=[]):
-    global vocab
-    global max_len
+def _build_vocab(data_name, train_dataset, test_dataset=None):
     all_token = []
 
     for i, line in enumerate(train_dataset):
@@ -72,12 +71,12 @@ def _build_vocab(data_name, train_dataset, test_dataset=[]):
     vocab.set_embedding(nlp.embedding.create('Word2Vec', source='GoogleNews-vectors-negative300'))
     for line in vocab.embedding._idx_to_token:
         if (vocab.embedding[line] == nd.zeros(300)).sum() == 300:
-            vocab.embedding[line]=nd.random.uniform(-0.25, 0.25, 300)
+            vocab.embedding[line] = nd.random.uniform(-0.25, 0.25, 300)
     vocab.embedding['<unk>'] = nd.zeros(300)
     vocab.embedding['<pad>'] = nd.zeros(300)
     vocab.embedding['<bos>'] = nd.zeros(300)
     vocab.embedding['<eos>'] = nd.zeros(300)
-    #return vocab, max_len
+    return vocab, max_len
 
 # Dataset preprocessing
 def preprocess(x):
@@ -93,10 +92,10 @@ def preprocess(x):
 def get_length(x):
     return float(len(x[0]))
 
-def preprocess_dataset(dataset):
+def preprocess_dataset(dataset, vocab, max_len):
     start = time.time()
     pool = mp.Pool(8)
-    dataset = pool.map(preprocess, dataset)
+    dataset = pool.map(preprocess, dataset, vocab, max_len)
     lengths = gluon.data.SimpleDataset(pool.map(get_length, dataset))
     end = time.time()
     print('Done! Tokenizing Time={:.2f}s, #Sentences={}'.format(end - start, len(dataset)))
@@ -105,12 +104,12 @@ def preprocess_dataset(dataset):
 def load_dataset(data_name):
     if data_name == 'MR' or data_name == 'Subj':
         train_dataset, output_size = _load_file(data_name)
-        _build_vocab(data_name, train_dataset)
+        vocab, max_len = _build_vocab(data_name, train_dataset)
         train_dataset, train_data_lengths = preprocess_dataset(train_dataset)
         return vocab, max_len, output_size, train_dataset, train_data_lengths
     else:
         train_dataset, test_dataset, output_size = _load_file(data_name)
-        _build_vocab(data_name, train_dataset, test_dataset)
+        vocab, max_len = _build_vocab(data_name, train_dataset, test_dataset)
         train_dataset, train_data_lengths = preprocess_dataset(train_dataset)
         test_dataset, test_data_lengths = preprocess_dataset(test_dataset)
         return vocab, max_len, output_size, train_dataset, train_data_lengths, test_dataset, \
