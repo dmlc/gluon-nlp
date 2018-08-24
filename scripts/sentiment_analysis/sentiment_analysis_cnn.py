f3704aa0c07122f738ed13dc040aff221d70c27e
xiaotinghe <master@hexiaoting.com>
2018-08-24 21:42:27 +0800

    Update sentiment_analysis_cnn.py

--- scripts/sentiment_analysis/sentiment_analysis_cnn.py
+++ scripts/sentiment_analysis/sentiment_analysis_cnn.py
@@ -35,8 +35,8 @@ import mxnet as mx
 from mxnet import nd, gluon, autograd
 from mxnet.gluon.data import DataLoader
 
-import .process
-import .textCNN
+import process
+import textCNN
 
 
 np.random.seed(3435)
