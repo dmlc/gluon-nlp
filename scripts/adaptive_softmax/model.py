68a31ee517adab71d1bc8e3fab304326b5509937
Duo Zeng <33174978+dofuer99@users.noreply.github.com>
2018-09-08 11:22:37 +0200

    Update model.py

--- scripts/adaptive_softmax/model.py
+++ scripts/adaptive_softmax/model.py
@@ -1,10 +1,58 @@
+# coding: utf-8
+
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
 from mxnet.gluon import nn, rnn
 import mxnet as mx
 from adaptive_softmax import *
 
 class LanguageModel(gluon.Block):
+    """LanguageModel for adaptive softmax and regular full softmax.
+    We implement the adaptive softmax proposed in the following work:
+        @article{grave2016efficient,
+                 title={Efficient softmax approximation for GPUs},
+                 author={Grave, Edouard and Joulin, Armand and Ciss{\'e}, Moustapha and Grangier, David and J{\'e}gou, Herv{\'e}},
+                 journal={arXiv preprint arXiv:1609.04309},
+                 year={2016}
+        }
+    
+    Parameters:
+    ----------
+    vocab_size: int
+       the size of the vocabulary.
+    num_embed: int
+       the size of the embedding layer.
+    num_hidden: int
+       the size of the hidden layer.
+    num_layers: int
+       the number of hidden layers.
+    dropout: float
+       the chance of one connection to be ignored. 
+    adaptive_softmax: bool
+       If it is "True", the model uses adaptive softmax and the function "forward" will be called.
+       If it is "False", the model uses regular full softmax and the the function "log_prob" will be called.
+    ctx:
+       Calculation is based on mx.gpu(0) or mx.cpu(0)
+    cutoff: list or np.array
+       Build clusters for adaptive softmax.       
+    """
     def __init__(self, vocab_size, num_embed, num_hidden, num_layers, dropout=0.0,
-            adaptive_softmax=True, context=mx.gpu(0), cutoff=[2000], **kwargs):
+            adaptive_softmax=True, ctx=mx.gpu(0), cutoff=[2000], **kwargs):
         super(LanguageModel, self).__init__(**kwargs)
         
         with self.name_scope():
