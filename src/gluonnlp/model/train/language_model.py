cd759ca9ba91a3ba303fb2a7306fd9df71ee5c53
Cong <18083731+congchan@users.noreply.github.com>
2018-10-24 06:03:47 +0800

    add mixture of softmaxes language model (#194)
    1. add MoS model for training and evaluation
    2. add unit tests
    3. add training scripts
    4. add pre-trained model

--- src/gluonnlp/model/train/language_model.py
+++ src/gluonnlp/model/train/language_model.py
@@ -17,7 +17,7 @@
 # specific language governing permissions and limitations
 # under the License.
 """Language models for training."""
-__all__ = ['AWDRNN', 'StandardRNN', 'BigRNN']
+__all__ = ['AWDRNN', 'StandardRNN', 'BigRNN', 'MoSRNN']
 
 from mxnet import init, nd, autograd
 from mxnet.gluon import nn, Block, contrib, rnn
@@ -425,3 +425,171 @@ class BigRNN(Block):
         out = out.reshape((length, batch_size, -1))
         new_target = new_target.reshape((length, batch_size))
         return out, out_states, new_target
+
+
+class MoSRNN(Block):
+    """Mos language model by Zhilin Yang, Zihang Dai, Ruslan Salakhutdinov, William W. Cohen.
+
+    Reference: https://github.com/zihangdai/mos
+
+    License: MIT License
+
+    Parameters
+    ----------
+    mode : str
+        The type of RNN to use. Options are 'lstm', 'gru', 'rnn_tanh', 'rnn_relu'.
+    vocab_size : int
+        Size of the input vocabulary.
+    embed_size : int
+        Dimension of embedding vectors.
+    hidden_size : int
+        Number of hidden units for RNN.
+    hidden_size_last : int
+        Number of last hidden units for RNN.
+    num_layers : int
+        Number of RNN layers.
+    tie_weights : bool, default False
+        Whether to tie the weight matrices of output dense layer and input embedding layer.
+    dropout : float
+        Dropout rate to use for encoder output.
+    weight_drop : float
+        Dropout rate to use on encoder h2h weights.
+    drop_h : float
+        Dropout rate to on the output of intermediate layers of encoder.
+    drop_i : float
+        Dropout rate to on the output of embedding.
+    drop_e : float
+        Dropout rate to use on the embedding layer.
+    drop_l : float
+        Dropout rate to use on the latent layer.
+    num_experts : int
+        Number of softmax.
+    """
+    def __init__(self, mode, vocab_size, embed_size=300, hidden_size=1150, hidden_size_last=650,
+                 num_layers=3, tie_weights=True, dropout=0.4, weight_drop=0.5, drop_h=0.2,
+                 drop_i=0.55, drop_e=0.1, drop_l=0.29, num_experts=15, **kwargs):
+        super(MoSRNN, self).__init__(**kwargs)
+        self._mode = mode
+        self._vocab_size = vocab_size
+        self._embed_size = embed_size
+        self._hidden_size = hidden_size
+        self._hidden_size_last = hidden_size_last
+        self._num_layers = num_layers
+        self._dropout = dropout
+        self._drop_h = drop_h
+        self._drop_i = drop_i
+        self._drop_e = drop_e
+        self._drop_l = drop_l
+        self._num_experts = num_experts
+        self._weight_drop = weight_drop
+        self._tie_weights = tie_weights
+
+        with self.name_scope():
+            self.embedding = self._get_embedding()
+            self.encoder = self._get_encoder()
+            self.prior = self._get_prior()
+            self.latent = self._get_latent()
+            self.decoder = self._get_decoder()
+
+    def _get_embedding(self):
+        embedding = nn.HybridSequential()
+        with embedding.name_scope():
+            embedding_block = nn.Embedding(self._vocab_size, self._embed_size,
+                                           weight_initializer=init.Uniform(0.1))
+            if self._drop_e:
+                apply_weight_drop(embedding_block, 'weight', self._drop_e, axes=(1,))
+            embedding.add(embedding_block)
+            if self._drop_i:
+                embedding.add(nn.Dropout(self._drop_i, axes=(0,)))
+        return embedding
+
+    def _get_encoder(self):
+        encoder = nn.Sequential()
+        with encoder.name_scope():
+            for l in range(self._num_layers):
+                encoder.add(_get_rnn_layer(self._mode, 1, self._embed_size if l == 0 else
+                                           self._hidden_size, self._hidden_size if
+                                           l != self._num_layers - 1 else self._hidden_size_last,
+                                           0, self._weight_drop))
+        return encoder
+
+    def _get_prior(self):
+        prior = nn.HybridSequential()
+        with prior.name_scope():
+            prior.add(nn.Dense(self._num_experts, in_units=self._hidden_size_last,
+                               use_bias=False, flatten=False))
+        return prior
+
+    def _get_latent(self):
+        latent = nn.HybridSequential()
+        with latent.name_scope():
+            latent.add(nn.Dense(self._num_experts * self._embed_size, 'tanh',
+                                in_units=self._hidden_size_last, flatten=False))
+        return latent
+
+    def _get_decoder(self):
+        output = nn.HybridSequential()
+        with output.name_scope():
+            if self._tie_weights:
+                output.add(nn.Dense(self._vocab_size, flatten=False,
+                                    params=self.embedding[0].params))
+            else:
+                output.add(nn.Dense(self._vocab_size, flatten=False))
+        return output
+
+    def begin_state(self, *args, **kwargs):
+        return [c.begin_state(*args, **kwargs) for c in self.encoder]
+
+    def forward(self, inputs, begin_state=None): # pylint: disable=arguments-differ
+        """Implement the forward computation that the awd language model and cache model use.
+
+        Parameters
+        -----------
+        inputs : NDArray
+            input tensor with shape `(sequence_length, batch_size)`
+            when `layout` is "TNC".
+        begin_state : list
+            initial recurrent state tensor with length equals to num_layers.
+            the initial state with shape `(1, batch_size, num_hidden)`
+
+        Returns
+        --------
+        out: NDArray
+            output tensor with shape `(sequence_length, batch_size, input_size)`
+            when `layout` is "TNC".
+        out_states: list
+            output recurrent state tensor with length equals to num_layers.
+            the state with shape `(1, batch_size, num_hidden)`
+        encoded_raw: list
+            The list of outputs of the model's encoder with length equals to num_layers.
+            the shape of every encoder's output `(sequence_length, batch_size, num_hidden)`
+        encoded_dropped: list
+            The list of outputs with dropout of the model's encoder with length equals
+            to num_layers. The shape of every encoder's dropped output
+            `(sequence_length, batch_size, num_hidden)`
+        """
+        encoded = self.embedding(inputs)
+        if not begin_state:
+            begin_state = self.begin_state(batch_size=inputs.shape[1])
+        out_states = []
+        encoded_raw = []
+        encoded_dropped = []
+        for i, (e, s) in enumerate(zip(self.encoder, begin_state)):
+            encoded, state = e(encoded, s)
+            encoded_raw.append(encoded)
+            out_states.append(state)
+            if self._drop_h and i != len(self.encoder)-1:
+                encoded = nd.Dropout(encoded, p=self._drop_h, axes=(0,))
+                encoded_dropped.append(encoded)
+        if self._dropout:
+            encoded = nd.Dropout(encoded, p=self._dropout, axes=(0,))
+        encoded_dropped.append(encoded)
+        latent = nd.Dropout(self.latent(encoded), p=self._drop_l, axes=(0,))
+        logit = self.decoder(latent.reshape(-1, self._embed_size))
+        prior_logit = self.prior(encoded).reshape(-1, self._num_experts)
+        prior = nd.softmax(prior_logit)
+        prob = nd.softmax(logit.reshape(-1, self._vocab_size))
+        prob = prob.reshape(-1, self._num_experts, self._vocab_size)
+        prob = (prob * prior.expand_dims(2).broadcast_to(prob.shape)).sum(axis=1)
+        out = nd.log(nd.add(prob, 1e-8)).reshape(-1, inputs.shape[1], self._vocab_size)
+        return out, out_states, encoded_raw, encoded_dropped
