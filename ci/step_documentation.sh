0083032971e46c10f6854bfb1420b246d9f4caaa
Sheng Zha <zhasheng@amazon.com>
2018-10-23 16:32:30 -0700

    update CI

--- ci/step_documentation.sh
+++ ci/step_documentation.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-gpu=$(($EXECUTOR_NUMBER % 4))
 set -ex
 export LD_LIBRARY_PATH=/usr/local/cuda/lib64
 source ci/prepare_clean_env.sh doc
 ci/install_dep.sh
-export CUDA_VISIBLE_DEVICES=${gpu}
+export CUDA_VISIBLE_DEVICES=$EXECUTOR_NUMBER
 make clean_doc
 make docs
 set +ex
