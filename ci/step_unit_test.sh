06ee6cf64d8879bb952112c92e79dcb281d5ce4b
Sheng Zha <szha@users.noreply.github.com>
2018-10-23 19:17:30 -0700

    update CI (#375)

--- ci/step_unit_test.sh
+++ ci/step_unit_test.sh
@@ -2,12 +2,11 @@
 lang=$1
 branch=$2
 capture_flag=$3
-gpu=$(($EXECUTOR_NUMBER % 4))
 set -ex
 export LD_LIBRARY_PATH=/usr/local/cuda/lib64
 source ci/prepare_clean_env.sh ${lang}
 ci/install_dep.sh
-export CUDA_VISIBLE_DEVICES=${gpu}
+export CUDA_VISIBLE_DEVICES=$EXECUTOR_NUMBER
 py.test -v ${capture_flag} -n 4 -m "not serial" --durations=50 --cov=gluonnlp tests/unittest
 py.test -v ${capture_flag} -n 0 -m "serial" --durations=50 --cov=gluonnlp tests/unittest
 ci/codecov.sh -c -F ${branch},${lang} -n unittests
