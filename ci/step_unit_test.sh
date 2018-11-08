d49a7896ae92307cf3c930f2eb2e3d516a278fe7
Leonard Lausen <leonard@lausen.nl>
2018-11-08 18:44:41 +0800

    Adopt src/gluonnlp folder layout (#390)
    See: https://hynek.me/articles/testing-packaging/

--- ci/step_unit_test.sh
+++ ci/step_unit_test.sh
@@ -7,10 +7,12 @@ export LD_LIBRARY_PATH=/usr/local/cuda/lib64
 source ci/prepare_clean_env.sh ${lang}
 ci/install_dep.sh
 export CUDA_VISIBLE_DEVICES=$EXECUTOR_NUMBER
-py.test -v ${capture_flag} -n 4 -m "not serial" --durations=50 --cov=gluonnlp tests/unittest
-py.test -v ${capture_flag} -n 0 -m "serial" --durations=50 --cov=gluonnlp tests/unittest
-ci/codecov.sh -c -F ${branch},${lang} -n unittests
-py.test -v ${capture_flag} -n 4 -m "not serial" --durations=50 --cov=gluonnlp --cov=scripts scripts
-py.test -v ${capture_flag} -n 0 -m "serial" --durations=50 --cov=gluonnlp --cov=scripts scripts
-ci/codecov.sh -c -F ${branch},${lang} -n integration
+pytest -v ${capture_flag} -n 4 -m "not serial" --durations=50 tests/unittest --cov gluonnlp
+pytest -v ${capture_flag} -n 0 -m "serial" --durations=50 tests/unittest --cov gluonnlp
+coverage xml
+ci/codecov.sh -c -F ${branch},${lang} -n unittests -f coverage.xml
+pytest -v ${capture_flag} -n 4 -m "not serial" --durations=50 scripts --cov scripts
+pytest -v ${capture_flag} -n 0 -m "serial" --durations=50 scripts --cov scripts
+coverage xml
+ci/codecov.sh -c -F ${branch},${lang} -n integration -f coverage.xml
 set +ex
