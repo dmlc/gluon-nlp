527b23d192e7f90f511a39fd6c63547635c60fbf
Leonard Lausen <leonard@lausen.nl>
2018-11-08 12:45:16 +0000

    Seperately upload serial / not serial coverage reports

--- ci/step_unit_test.sh
+++ ci/step_unit_test.sh
@@ -8,11 +8,15 @@ source ci/prepare_clean_env.sh ${lang}
 ci/install_dep.sh
 export CUDA_VISIBLE_DEVICES=$EXECUTOR_NUMBER
 pytest -v ${capture_flag} -n 4 -m "not serial" --durations=50 tests/unittest --cov gluonnlp
+coverage xml
+ci/codecov.sh -c -F ${branch},${lang},notserial -n unittests -f coverage.xml
 pytest -v ${capture_flag} -n 0 -m "serial" --durations=50 tests/unittest --cov gluonnlp
 coverage xml
-ci/codecov.sh -c -F ${branch},${lang} -n unittests -f coverage.xml
+ci/codecov.sh -c -F ${branch},${lang},serial -n unittests -f coverage.xml
 pytest -v ${capture_flag} -n 4 -m "not serial" --durations=50 scripts --cov scripts
+coverage xml
+ci/codecov.sh -c -F ${branch},${lang},notserial -n integration -f coverage.xml
 pytest -v ${capture_flag} -n 0 -m "serial" --durations=50 scripts --cov scripts
 coverage xml
-ci/codecov.sh -c -F ${branch},${lang} -n integration -f coverage.xml
+ci/codecov.sh -c -F ${branch},${lang},serial -n integration -f coverage.xml
 set +ex
