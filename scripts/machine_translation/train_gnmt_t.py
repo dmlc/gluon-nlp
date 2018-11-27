8d2dcc53b39be30836acda57569baf5de4461052
pengxin99 <yc316ypx@126.com>
2018-11-27 09:44:41 +0800

    support multi gpus

--- scripts/machine_translation/train_gnmt_t.py
+++ scripts/machine_translation/train_gnmt_t.py
@@ -43,11 +43,20 @@ import logging
 import numpy as np
 import mxnet as mx
 from mxnet import gluon
+<<<<<<< HEAD
+from mxnet.gluon.data import ArrayDataset, SimpleDataset
+from mxnet.gluon.data import DataLoader
+from gluonnlp.data import ShardedDataLoader
+import gluonnlp.data.batchify as btf
+from gluonnlp.data import ConstWidthBucket, LinearWidthBucket, ExpWidthBucket, \
+    FixedBucketSampler, IWSLT2015, WMT2016BPE
+from gluonnlp.model import BeamSearchScorer
+=======
 import gluonnlp as nlp
 
-from gluonnlp.model.translation import NMTModel
+>>>>>>> upstream/master
 from gnmt import get_gnmt_encoder_decoder
-from translation import BeamSearchTranslator
+from translation import NMTModel, BeamSearchTranslator
 from loss import SoftmaxCEMaskedLoss
 from utils import logging_config
 from bleu import compute_bleu
@@ -109,6 +118,143 @@ print(args)
 logging_config(args.save_dir)
 
 
+<<<<<<< HEAD
+def cache_dataset(dataset, prefix):
+    """Cache the processed npy dataset  the dataset into a npz
+
+    Parameters
+    ----------
+    dataset : SimpleDataset
+    file_path : str
+    """
+    if not os.path.exists(_C.CACHE_PATH):
+        os.makedirs(_C.CACHE_PATH)
+    src_data = np.array([ele[0] for ele in dataset])
+    tgt_data = np.array([ele[1] for ele in dataset])
+    np.savez(os.path.join(_C.CACHE_PATH, prefix + '.npz'), src_data=src_data, tgt_data=tgt_data)
+
+
+def load_cached_dataset(prefix):
+    cached_file_path = os.path.join(_C.CACHE_PATH, prefix + '.npz')
+    if os.path.exists(cached_file_path):
+        print('Load cached data from {}'.format(cached_file_path))
+        dat = np.load(cached_file_path)
+        return ArrayDataset(np.array(dat['src_data']), np.array(dat['tgt_data']))
+    else:
+        return None
+
+
+class TrainValDataTransform(object):
+    """Transform the machine translation dataset.
+
+    Clip source and the target sentences to the maximum length. For the source sentence, append the
+    EOS. For the target sentence, append BOS and EOS.
+
+    Parameters
+    ----------
+    src_vocab : Vocab
+    tgt_vocab : Vocab
+    src_max_len : int
+    tgt_max_len : int
+    """
+    def __init__(self, src_vocab, tgt_vocab, src_max_len, tgt_max_len):
+        self._src_vocab = src_vocab
+        self._tgt_vocab = tgt_vocab
+        self._src_max_len = src_max_len
+        self._tgt_max_len = tgt_max_len
+
+    def __call__(self, src, tgt):
+        if self._src_max_len > 0:
+            src_sentence = self._src_vocab[src.split()[:self._src_max_len]]
+        else:
+            src_sentence = self._src_vocab[src.split()]
+        if self._tgt_max_len > 0:
+            tgt_sentence = self._tgt_vocab[tgt.split()[:self._tgt_max_len]]
+        else:
+            tgt_sentence = self._tgt_vocab[tgt.split()]
+        src_sentence.append(self._src_vocab[self._src_vocab.eos_token])
+        tgt_sentence.insert(0, self._tgt_vocab[self._tgt_vocab.bos_token])
+        tgt_sentence.append(self._tgt_vocab[self._tgt_vocab.eos_token])
+        src_npy = np.array(src_sentence, dtype=np.int32)
+        tgt_npy = np.array(tgt_sentence, dtype=np.int32)
+        return src_npy, tgt_npy
+
+
+def process_dataset(dataset, src_vocab, tgt_vocab, src_max_len=-1, tgt_max_len=-1):
+    start = time.time()
+    dataset_processed = dataset.transform(TrainValDataTransform(src_vocab, tgt_vocab,
+                                                                src_max_len,
+                                                                tgt_max_len), lazy=False)
+    end = time.time()
+    print('Processing Time spent: {}'.format(end - start))
+    return dataset_processed
+
+
+def load_translation_data(dataset, src_lang='en', tgt_lang='vi'):
+    """Load translation dataset
+
+    Parameters
+    ----------
+    dataset : str
+    src_lang : str, default 'en'
+    tgt_lang : str, default 'vi'
+
+    Returns
+    -------
+
+    """
+    common_prefix = 'IWSLT2015_{}_{}_{}_{}'.format(src_lang, tgt_lang,
+                                                   args.src_max_len, args.tgt_max_len)
+    if dataset == 'IWSLT2015':
+        data_train = IWSLT2015('train', src_lang=src_lang, tgt_lang=tgt_lang)
+        data_val = IWSLT2015('val', src_lang=src_lang, tgt_lang=tgt_lang)
+        data_test = IWSLT2015('test', src_lang=src_lang, tgt_lang=tgt_lang)
+
+    elif dataset == 'TOY':
+        common_prefix = 'TOY_{}_{}_{}_{}'.format(src_lang, tgt_lang,
+                                                 args.src_max_len, args.tgt_max_len)
+        data_train = TOY('train', src_lang=src_lang, tgt_lang=tgt_lang)
+        data_val = TOY('val', src_lang=src_lang, tgt_lang=tgt_lang)
+        data_test = TOY('test', src_lang=src_lang, tgt_lang=tgt_lang)
+
+    elif dataset == 'WMT2016BPE':
+        print("|--------------------------------|")
+        common_prefix = 'WMT2016BPE_{}_{}_{}_{}'.format(src_lang, tgt_lang,
+                                                 args.src_max_len, args.tgt_max_len)
+        data_train = WMT2016BPE('train', src_lang=src_lang, tgt_lang=tgt_lang)
+        data_val = WMT2016BPE('newstest2016', src_lang=src_lang, tgt_lang=tgt_lang)
+        data_test = WMT2016BPE('newstest2015', src_lang=src_lang, tgt_lang=tgt_lang)
+
+    else:
+        raise NotImplementedError
+
+    src_vocab, tgt_vocab = data_train.src_vocab, data_train.tgt_vocab
+    data_train_processed = load_cached_dataset(common_prefix + '_train')
+    if not data_train_processed:
+        data_train_processed = process_dataset(data_train, src_vocab, tgt_vocab,
+                                               args.src_max_len, args.tgt_max_len)
+        cache_dataset(data_train_processed, common_prefix + '_train')
+    data_val_processed = load_cached_dataset(common_prefix + '_val')
+    if not data_val_processed:
+        data_val_processed = process_dataset(data_val, src_vocab, tgt_vocab)
+        cache_dataset(data_val_processed, common_prefix + '_val')
+    data_test_processed = load_cached_dataset(common_prefix + '_test')
+    if not data_test_processed:
+        data_test_processed = process_dataset(data_test, src_vocab, tgt_vocab)
+        cache_dataset(data_test_processed, common_prefix + '_test')
+    fetch_tgt_sentence = lambda src, tgt: tgt.split()
+    val_tgt_sentences = list(data_val.transform(fetch_tgt_sentence))
+    test_tgt_sentences = list(data_test.transform(fetch_tgt_sentence))
+    return data_train_processed, data_val_processed, data_test_processed, \
+           val_tgt_sentences, test_tgt_sentences, src_vocab, tgt_vocab
+
+
+def get_data_lengths(dataset):
+    return list(dataset.transform(lambda srg, tgt: (len(srg), len(tgt))))
+
+
+=======
+>>>>>>> upstream/master
 data_train, data_val, data_test, val_tgt_sentences, test_tgt_sentences, src_vocab, tgt_vocab\
     = dataprocessor.load_translation_data(dataset=args.dataset, bleu='tweaked', args=args)
 
@@ -116,26 +262,30 @@ dataprocessor.write_sentences(val_tgt_sentences, os.path.join(args.save_dir, 'va
 dataprocessor.write_sentences(test_tgt_sentences, os.path.join(args.save_dir, 'test_gt.txt'))
 
 data_train = data_train.transform(lambda src, tgt: (src, tgt, len(src), len(tgt)), lazy=False)
+<<<<<<< HEAD
+data_val = SimpleDataset([(ele[0], ele[1], len(ele[0]), len(ele[1]), i)
+                          for i, ele in enumerate(data_val)])
+data_test = SimpleDataset([(ele[0], ele[1], len(ele[0]), len(ele[1]), i)
+                           for i, ele in enumerate(data_test)])
+
+# set context of cpu or gpu.
+if args.gpus is None:
+=======
 data_val = gluon.data.SimpleDataset([(ele[0], ele[1], len(ele[0]), len(ele[1]), i)
                                      for i, ele in enumerate(data_val)])
 data_test = gluon.data.SimpleDataset([(ele[0], ele[1], len(ele[0]), len(ele[1]), i)
                                       for i, ele in enumerate(data_test)])
+if args.gpu is None:
+>>>>>>> upstream/master
+    ctx = mx.cpu()
+    context = [ctx]
+elif args.gpus == "0":
+    ctx = mx.gpu(0)
+    context = [ctx]
+else:
+    ctx = mx.gpu(0)
+    context = [mx.gpu(int(x)) for x in args.gpus.split(',')]
 
-# if args.gpus is None:
-#     ctx = mx.cpu()
-#     context = [mx.cpu(0), mx.cpu(1)]
-#     print('Use CPU')
-# elif args.gpus == "0":
-#     ctx = mx.gpu(0)
-#     context = [ctx]
-# else:
-#     ctx = mx.gpu(0)
-#     context = [mx.gpu(int(x)) for x in arg    s.gpus.split(',')]
-
-
-context = [mx.cpu()] if args.gpus is None or args.gpus == '' else \
-    [mx.gpu(int(x)) for x in args.gpus.split(',')]
-ctx = context[0]
 
 # load GNMT model
 encoder, decoder = get_gnmt_encoder_decoder(hidden_size=args.num_hidden,
@@ -143,18 +293,15 @@ encoder, decoder = get_gnmt_encoder_decoder(hidden_size=args.num_hidden,
                                             num_layers=args.num_layers,
                                             num_bi_layers=args.num_bi_layers,
                                             attention_cell=args.attention_type)
-
 model = NMTModel(src_vocab=src_vocab, tgt_vocab=tgt_vocab, encoder=encoder, decoder=decoder,
                  embed_size=args.num_embedding, prefix='gnmt_')
 
-# save_path = os.path.join(args.save_dir, 'valid_best.params' )
-# logging.info('|-----load epoch parameters from {}'.format(save_path))
-# model.load_parameters(save_path, context)
 
-model.initialize(init=mx.init.Uniform(0.1), ctx=ctx)
+model.initialize(init=mx.init.Uniform(0.1), ctx=context)
 static_alloc = True
 model.hybridize(static_alloc=static_alloc)
 logging.info(model)
+
 
 translator = BeamSearchTranslator(model=model, beam_size=args.beam_size,
                                   scorer=nlp.model.BeamSearchScorer(alpha=args.lp_alpha,
@@ -217,15 +364,67 @@ def train():
     """Training function."""
     trainer = gluon.Trainer(model.collect_params(), args.optimizer, {'learning_rate': args.lr})
 
+<<<<<<< HEAD
+    train_batchify_fn = btf.Tuple(btf.Pad(), btf.Pad(),
+                                  btf.Stack(dtype='float32'), btf.Stack(dtype='float32'))
+    test_batchify_fn = btf.Tuple(btf.Pad(), btf.Pad(),
+                                 btf.Stack(dtype='float32'), btf.Stack(dtype='float32'),
+                                 btf.Stack())
+    if args.bucket_scheme == 'constant':
+        bucket_scheme = ConstWidthBucket()
+    elif args.bucket_scheme == 'linear':
+        bucket_scheme = LinearWidthBucket()
+    elif args.bucket_scheme == 'exp':
+        bucket_scheme = ExpWidthBucket(bucket_len_step=1.2)
+    else:
+        raise NotImplementedError
+    # put train data into bucket
+    train_batch_sampler = FixedBucketSampler(lengths=data_train_lengths,
+                                             batch_size=args.batch_size,
+                                             num_buckets=args.num_buckets,
+                                             ratio=args.bucket_ratio,
+                                             shuffle=True,
+                                             num_shards=len(context),
+                                             bucket_scheme=bucket_scheme)
+    logging.info('Train Batch Sampler:\n{}'.format(train_batch_sampler.stats()))
+    train_data_loader = ShardedDataLoader(data_train,
+                                   batch_sampler=train_batch_sampler,
+                                   batchify_fn=train_batchify_fn,
+                                   num_workers=8)
+    # put val data into bucket
+    val_batch_sampler = FixedBucketSampler(lengths=data_val_lengths,
+                                           batch_size=args.test_batch_size,
+                                           num_buckets=args.num_buckets,
+                                           ratio=args.bucket_ratio,
+                                           shuffle=False)
+    logging.info('Valid Batch Sampler:\n{}'.format(val_batch_sampler.stats()))
+    val_data_loader = DataLoader(data_val,
+                                 batch_sampler=val_batch_sampler,
+                                 batchify_fn=test_batchify_fn,
+                                 num_workers=8)
+    test_batch_sampler = FixedBucketSampler(lengths=data_test_lengths,
+                                            batch_size=args.test_batch_size,
+                                            num_buckets=args.num_buckets,
+                                            ratio=args.bucket_ratio,
+                                            shuffle=False)
+    logging.info('Test Batch Sampler:\n{}'.format(test_batch_sampler.stats()))
+    test_data_loader = DataLoader(data_test,
+                                  batch_sampler=test_batch_sampler,
+                                  batchify_fn=test_batchify_fn,
+                                  num_workers=8)
+=======
     train_data_loader, val_data_loader, test_data_loader \
-        = dataprocessor.make_dataloader(data_train, data_val, data_test, args, num_shards=len(context))
+        = dataprocessor.make_dataloader(data_train, data_val, data_test, args)
 
+>>>>>>> upstream/master
     best_valid_bleu = 0.0
     for epoch_id in range(args.epochs):
+
         log_avg_loss = 0
         log_avg_gnorm = 0
         log_wc = 0
         log_start_time = time.time()
+
         for batch_id, seqs \
                 in enumerate(train_data_loader):
 
@@ -244,15 +443,14 @@ def train():
                     loss = loss_function(out, tgt_seq[:, 1:], tgt_valid_length - 1).mean()
                     loss = loss * (tgt_seq.shape[1] - 1) / (tgt_valid_length - 1).mean()
                     LS.append(loss)
-
+            print("|---- context is :" + str(context))
+            print("|---- LS is :"+str(LS))
             for L in LS:
                 L.backward()
 
             grads = [p.grad(ctx) for p in model.collect_params().values()]
             gnorm = gluon.utils.clip_global_norm(grads, args.clip)
             trainer.step(1)
-            src_wc = src_valid_length.sum().asscalar()
-            tgt_wc = (tgt_valid_length - 1).sum().asscalar()
 
             # calculate the loss at all contexts
             step_loss = sum([L.asscalar() for L in LS])
@@ -260,8 +458,10 @@ def train():
 
             log_avg_gnorm += gnorm
             log_wc += src_wc + tgt_wc
+
             if (batch_id + 1) % args.log_interval == 0:
                 wps = log_wc / (time.time() - log_start_time)
+
                 logging.info('[Epoch {} Batch {}/{}] loss={:.4f}, ppl={:.4f}, gnorm={:.4f}, '
                              'throughput={:.2f}K wps, wc={:.2f}K'
                              .format(epoch_id, batch_id + 1, len(train_data_loader),
@@ -269,33 +469,48 @@ def train():
                                      np.exp(log_avg_loss / args.log_interval),
                                      log_avg_gnorm / args.log_interval,
                                      wps / 1000, log_wc / 1000))
+
                 log_start_time = time.time()
                 log_avg_loss = 0
                 log_avg_gnorm = 0
                 log_wc = 0
+
         valid_loss, valid_translation_out = evaluate(val_data_loader)
         valid_bleu_score, _, _, _, _ = compute_bleu([val_tgt_sentences], valid_translation_out)
         logging.info('[Epoch {}] valid Loss={:.4f}, valid ppl={:.4f}, valid bleu={:.2f}'
                      .format(epoch_id, valid_loss, np.exp(valid_loss), valid_bleu_score * 100))
+
         test_loss, test_translation_out = evaluate(test_data_loader)
         test_bleu_score, _, _, _, _ = compute_bleu([test_tgt_sentences], test_translation_out)
         logging.info('[Epoch {}] test Loss={:.4f}, test ppl={:.4f}, test bleu={:.2f}'
                      .format(epoch_id, test_loss, np.exp(test_loss), test_bleu_score * 100))
+<<<<<<< HEAD
+
+        write_sentences(valid_translation_out,
+                        os.path.join(args.save_dir, 'epoch{:d}_valid_out.txt').format(epoch_id))
+        write_sentences(test_translation_out,
+                        os.path.join(args.save_dir, 'epoch{:d}_test_out.txt').format(epoch_id))
+
+=======
         dataprocessor.write_sentences(valid_translation_out,
                                       os.path.join(args.save_dir,
                                                    'epoch{:d}_valid_out.txt').format(epoch_id))
         dataprocessor.write_sentences(test_translation_out,
                                       os.path.join(args.save_dir,
                                                    'epoch{:d}_test_out.txt').format(epoch_id))
+>>>>>>> upstream/master
         if valid_bleu_score > best_valid_bleu:
             best_valid_bleu = valid_bleu_score
             save_path = os.path.join(args.save_dir, 'valid_best.params')
             logging.info('Save best parameters to {}'.format(save_path))
             model.save_parameters(save_path)
+
         if epoch_id + 1 >= (args.epochs * 2) // 3:
             new_lr = trainer.learning_rate * args.lr_update_factor
             logging.info('Learning rate change to {}'.format(new_lr))
             trainer.set_learning_rate(new_lr)
+
+    # to show the best result at the end
     if os.path.exists(os.path.join(args.save_dir, 'valid_best.params')):
         model.load_parameters(os.path.join(args.save_dir, 'valid_best.params'))
     valid_loss, valid_translation_out = evaluate(val_data_loader)
