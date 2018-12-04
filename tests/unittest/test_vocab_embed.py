7d9bde5586cd4438bc8cfef5845d907dad059014
Leonard Lausen <leonard@lausen.nl>
2018-12-04 06:13:02 +0000

    Fix testcase under Py2

--- tests/unittest/test_vocab_embed.py
+++ tests/unittest/test_vocab_embed.py
@@ -1092,9 +1092,12 @@ def test_subword_function_ngramhashes():
     sf = nlp.vocab.create_subword_function('NGramHashes', ngrams=[3, 4, 5, 6],
                                            num_subwords=num_subwords)
 
+    assert set([8, 195, 271, 500, 201, 445, 379, 831, 617, 851]) == set(sf(['test'])[0])
     assert set([8, 195, 271, 500, 201, 445, 379, 831, 617, 851]) == set(sf([u'test'])[0])
     assert set([429, 793, 101, 334, 295, 474, 145, 524, 388, 790]) == set(sf([u'τεστ'])[0])
     assert 1669484008 == sf.fasttext_hash_asbytes('<te')
-    assert 2688791429 == sf.fasttext_hash_asbytes('<τε')
+    assert 1669484008 == sf.fasttext_hash_asbytes(u'<te')
+    assert 2688791429 == sf.fasttext_hash_asbytes(u'<τε')
     assert 1669484008 % num_subwords == next(iter(sf.subwords_to_indices(['<te'])))
-    assert 2688791429 % num_subwords == next(iter(sf.subwords_to_indices(['<τε'])))
+    assert 1669484008 % num_subwords == next(iter(sf.subwords_to_indices([u'<te'])))
+    assert 2688791429 % num_subwords == next(iter(sf.subwords_to_indices([u'<τε'])))
