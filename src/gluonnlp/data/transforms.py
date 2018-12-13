f92a19ce3840b52427ca3820eea8647f8968f77b
fiercex <fiercex@outlook.com>
2018-12-13 17:41:18 +0800

    add BERTTokenizer

--- src/gluonnlp/data/transforms.py
+++ src/gluonnlp/data/transforms.py
@@ -25,7 +25,7 @@ from __future__ import print_function
 
 __all__ = ['ClipSequence', 'PadSequence', 'SacreMosesTokenizer', 'NLTKMosesTokenizer',
            'SpacyTokenizer', 'SacreMosesDetokenizer', 'NLTKMosesDetokenizer', 'JiebaTokenizer',
-           'NLTKStanfordSegmenter', 'SentencepieceTokenizer', 'SentencepieceDetokenizer']
+           'NLTKStanfordSegmenter', 'SentencepieceTokenizer', 'SentencepieceDetokenizer', 'BERTTokenizer']
 
 import os
 import warnings
@@ -693,3 +693,253 @@ class SentencepieceDetokenizer(_SentencepieceProcessor):
             Detokenized text
         """
         return self._processor.DecodePieces(sample)
+
+
+
+
+class BERTTokenizer(object):
+    def __init__(self, vocab, do_lower_case=True, unk_token='[UNK]', max_input_chars_per_word=100):
+        self.vocab = vocab
+        self.do_lower_case = do_lower_case
+        self.unk_token = unk_token
+        self.max_input_chars_per_word = max_input_chars_per_word
+
+    def __call__(self,sample):
+        return self._tokenizer(sample)
+
+    def _tokenizer(self,text):
+        split_tokens = []
+        for token in self._BasicTokenizer(text):
+            for sub_token in self._WordpieceTokenizer(token):
+                split_tokens.append(sub_token)
+
+        return split_tokens
+
+    def _BasicTokenizer(self,text):
+        """Tokenizes a piece of text."""
+        text = self._convert_to_unicode(text)
+        text = self._clean_text(text)
+
+        # This was added on November 1st, 2018 for the multilingual and Chinese
+        # models. This is also applied to the English models now, but it doesn't
+        # matter since the English models were not trained on any Chinese data
+        # and generally don't have any Chinese data in them (there are Chinese
+        # characters in the vocabulary because Wikipedia does have some Chinese
+        # words in the English Wikipedia.).
+        text = self._tokenize_chinese_chars(text)
+        orig_tokens = self._whitespace_tokenize(text)
+        split_tokens = []
+        for token in orig_tokens:
+            if self.do_lower_case:
+                token = token.lower()
+                token = self._run_strip_accents(token)
+            split_tokens.extend(self._run_split_on_punc(token))
+
+        output_tokens = self._whitespace_tokenize(' '.join(split_tokens))
+        return output_tokens
+
+    def _WordpieceTokenizer(self,text):
+        """Tokenizes a piece of text into its word pieces.
+
+        This uses a greedy longest-match-first algorithm to perform tokenization
+        using the given vocabulary.
+
+        For example:
+          input = "unaffable"
+          output = ["un", "##aff", "##able"]
+
+        Args:
+          text: A single token or whitespace separated tokens. This should have
+            already been passed through `BasicTokenizer.
+
+        Returns:
+          A list of wordpiece tokens.
+        """
+
+        text = self._convert_to_unicode(text)
+
+        output_tokens = []
+        for token in self._whitespace_tokenize(text):
+            chars = list(token)
+            if len(chars) > self.max_input_chars_per_word:
+                output_tokens.append(self.unk_token)
+                continue
+
+            is_bad = False
+            start = 0
+            sub_tokens = []
+            while start < len(chars):
+                end = len(chars)
+                cur_substr = None
+                while start < end:
+                    substr = ''.join(chars[start:end])
+                    if start > 0:
+                        substr = '##' + substr
+                    if substr in self.vocab:
+                        cur_substr = substr
+                        break
+                    end -= 1
+                if cur_substr is None:
+                    is_bad = True
+                    break
+                sub_tokens.append(cur_substr)
+                start = end
+
+            if is_bad:
+                output_tokens.append(self.unk_token)
+            else:
+                output_tokens.extend(sub_tokens)
+        return output_tokens
+
+    def convert_tokens_to_ids(self,tokens):
+        """Converts a sequence of tokens into ids using the vocab."""
+        ids = []
+        for token in tokens:
+            ids.append(self.vocab[token])
+        return ids
+
+    def _clean_text(self, text):
+        """Performs invalid character removal and whitespace cleanup on text."""
+        output = []
+        for char in text:
+            cp = ord(char)
+            if cp == 0 or cp == 0xfffd or self._is_control(char):
+                continue
+            if self._is_whitespace(char):
+                output.append(' ')
+            else:
+                output.append(char)
+        return ''.join(output)
+
+    def _tokenize_chinese_chars(self, text):
+        """Adds whitespace around any CJK character."""
+        output = []
+        for char in text:
+            cp = ord(char)
+            if self._is_chinese_char(cp):
+                output.append(' ')
+                output.append(char)
+                output.append(' ')
+            else:
+                output.append(char)
+        return ''.join(output)
+
+    def _is_chinese_char(self, cp):
+        """Checks whether CP is the codepoint of a CJK character."""
+        # This defines a "chinese character" as anything in the CJK Unicode block:
+        #   https://en.wikipedia.org/wiki/CJK_Unified_Ideographs_(Unicode_block)
+        #
+        # Note that the CJK Unicode block is NOT all Japanese and Korean characters,
+        # despite its name. The modern Korean Hangul alphabet is a different block,
+        # as is Japanese Hiragana and Katakana. Those alphabets are used to write
+        # space-separated words, so they are not treated specially and handled
+        # like the all of the other languages.
+        if ((cp >= 0x4E00 and cp <= 0x9FFF)
+                or (cp >= 0x3400 and cp <= 0x4DBF)
+                or (cp >= 0x20000 and cp <= 0x2A6DF)
+                or (cp >= 0x2A700 and cp <= 0x2B73F)
+                or (cp >= 0x2B740 and cp <= 0x2B81F)
+                or (cp >= 0x2B820 and cp <= 0x2CEAF)
+                or (cp >= 0xF900 and cp <= 0xFAFF)
+                or (cp >= 0x2F800 and cp <= 0x2FA1F)):
+            return True
+
+        return False
+
+    def _run_strip_accents(self, text):
+        """Strips accents from a piece of text."""
+        text = unicodedata.normalize('NFD', text)
+        output = []
+        for char in text:
+            cat = unicodedata.category(char)
+            if cat == 'Mn':
+                continue
+            output.append(char)
+        return ''.join(output)
+
+    def _run_split_on_punc(self, text):
+        """Splits punctuation on a piece of text."""
+        chars = list(text)
+        i = 0
+        start_new_word = True
+        output = []
+        while i < len(chars):
+            char = chars[i]
+            if self._is_punctuation(char):
+                output.append([char])
+                start_new_word = True
+            else:
+                if start_new_word:
+                    output.append([])
+                start_new_word = False
+                output[-1].append(char)
+            i += 1
+
+        return [''.join(x) for x in output]
+
+    def _whitespace_tokenize(self,text):
+        """Runs basic whitespace cleaning and splitting on a piece of text."""
+        text = text.strip()
+        if not text:
+            return []
+        tokens = text.split()
+        return tokens
+
+    def _convert_to_unicode(self,text):
+        """Converts `text` to Unicode (if it's not already), assuming utf-8 input."""
+        if six.PY3:
+            if isinstance(text, str):
+                return text
+            elif isinstance(text, bytes):
+                return text.decode('utf-8', 'ignore')
+            else:
+                raise ValueError('Unsupported string type: %s' % (type(text)))
+        elif six.PY2:
+            if isinstance(text, str):
+                return text.decode('utf-8', 'ignore')
+            elif isinstance(text, unicode):  # noqa: F821
+                return text
+            else:
+                raise ValueError('Unsupported string type: %s' % (type(text)))
+        else:
+            raise ValueError('Not running on Python2 or Python 3?')
+
+    def _is_whitespace(self,char):
+        """Checks whether `chars` is a whitespace character."""
+        # \t, \n, and \r are technically contorl characters but we treat them
+        # as whitespace since they are generally considered as such.
+        if char in [' ', '\t', '\n', '\r']:
+            return True
+        cat = unicodedata.category(char)
+        if cat == 'Zs':
+            return True
+        return False
+
+    def _is_control(self,char):
+        """Checks whether `chars` is a control character."""
+        # These are technically control characters but we count them as whitespace
+        # characters.
+        if char in ['\t', '\n', '\r']:
+            return False
+        cat = unicodedata.category(char)
+        if cat.startswith('C'):
+            return True
+        return False
+
+    def _is_punctuation(self,char):
+        """Checks whether `chars` is a punctuation character."""
+        cp = ord(char)
+        # We treat all non-letter/number ASCII as punctuation.
+        # Characters such as "^", "$", and "`" are not in the Unicode
+        # Punctuation class but we treat them as punctuation anyways, for
+        # consistency.
+        group0 = cp >= 33 and cp <= 47
+        group1 = cp >= 58 and cp <= 64
+        group2 = cp >= 91 and cp <= 96
+        group3 = cp >= 123 and cp <= 126
+        if (group0 or group1 or group2 or group3):
+            return True
+        cat = unicodedata.category(char)
+        if cat.startswith('P'):
+            return True
+        return False
