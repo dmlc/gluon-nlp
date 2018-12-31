21a2a6777c1d0cbe0174f124a7859d1f0489d76f
Ubuntu <ubuntu@ip-172-31-87-67.ec2.internal>
2018-12-31 05:24:01 +0000

    runnable script

--- scripts/bert/run_pretraining.py
+++ scripts/bert/run_pretraining.py
@@ -33,7 +33,7 @@ This example shows how to pre-train a BERT model with Gluon NLP Toolkit.
 import sys
 import os
 
-os.environ['GLUON_MIN'] = '8'
+# os.environ['GLUON_MIN'] = '8'
 sys.path.insert(0, '/home/ubuntu/gluon-nlp/src/')
 sys.path.insert(0, '/home/ubuntu/mxnet/python/')
 
@@ -45,65 +45,48 @@ import mxnet as mx
 import time
 from mxnet import gluon
 from mxnet.gluon.data import ArrayDataset, DataLoader
+from gluonnlp.utils import clip_grad_global_norm
 from gluonnlp.model import bert_12_768_12
-from gluonnlp.data import SimpleDatasetStream
+from gluonnlp.data import SimpleDatasetStream, SplitSampler, NumpyDataset
 from tokenizer import FullTokenizer
 from dataset import MRPCDataset, ClassificationTransform
 
 parser = argparse.ArgumentParser(description='BERT pretraining example.')
-parser.add_argument('--epochs', type=int, default=40, help='Number of epochs')
+parser.add_argument('--num_steps', type=int, default=1000, help='Number of optimization steps')
 parser.add_argument('--dtype', type=str, default='float32', help='data dtype')
-parser.add_argument('--batch_size', type=int, default=32, help='Batch size per GPU.')
-parser.add_argument('--test_batch_size', type=int, default=8, help='Test batch size per GPU')
+parser.add_argument('--batch_size', type=int, default=8, help='Batch size per GPU.')
 parser.add_argument('--dataset_name', type=str, default='book_corpus_wiki_en_uncased',
                     help='The dataset from which the vocabulary is created. '
                          'Options include book_corpus_wiki_en_uncased, book_corpus_wiki_en_cased. '
                          'Default is book_corpus_wiki_en_uncased')
-parser.add_argument('--optimizer', type=str, default='bertadam', help='optimization algorithm')
 parser.add_argument('--load_ckpt', type=str, default=None, help='Load model from a checkpoint.')
 parser.add_argument('--pretrained', action='store_true',
                     help='Load the pretrained model released by Google.')
-parser.add_argument('--data', type=str, default=None,
+parser.add_argument('--data', type=str, required=True,
                     help='Path to training data.')
-parser.add_argument('--eval_data', type=str, default=None, help='Path to eval data.')
-parser.add_argument('--kv', type=str, default='device', help='Type of kvstore')
-parser.add_argument('--lr', type=float, default=1e-5, help='Initial learning rate')
+parser.add_argument('--lr', type=float, default=1e-4, help='Learning rate')
 parser.add_argument('--warmup_ratio', type=float, default=0.1,
                     help='ratio of warmup steps used in NOAM\'s stepsize schedule')
 parser.add_argument('--log_interval', type=int, default=10, help='report interval')
 parser.add_argument('--max_len', type=int, default=512, help='Maximum length of the sentence pairs')
-parser.add_argument('--dropout', type=float, default=0.1, help='Dropout rate')
-parser.add_argument('--num_gpus', type=int, help='Number of GPUs to use')
+parser.add_argument('--gpu', action='store_true', help='Whether to use GPU')
 parser.add_argument('--seed', type=int, default=0, help='Random seed')
 parser.add_argument('--do-training', action='store_true',
                     help='Whether to do training on the training set.')
-parser.add_argument('--do-eval', action='store_true',
-                    help='Whether to do evaluation on the eval set.')
-
-#parser.add_argument('--profile', action='store_true', help='whether to use gpu for finetuning')
-#parser.add_argument('--summary', action='store_true', help='whether to use gpu for finetuning')
 args = parser.parse_args()
-
-# random seed
-seed = args.seed
-np.random.seed(seed)
-random.seed(seed)
-mx.random.seed(seed)
-
 # logging
 logging.getLogger().setLevel(logging.INFO)
 logging.info(args)
 
-lr = args.lr
-
-def get_model(args, ctx):
+def get_model(ctx):
     # model
     pretrained = args.pretrained
     dataset = args.dataset_name
+    # TODO API
     model, vocabulary = bert_12_768_12(dataset_name=dataset,
-                                      pretrained=pretrained, ctx=ctx, use_pooler=True,
-                                      use_decoder=True, use_classifier=True, dropout=args.dropout,
-                                      embed_dropout=args.dropout, for_pretrain=True)
+                                       pretrained=pretrained, ctx=ctx, use_pooler=True,
+                                       use_decoder=True, use_classifier=True,
+                                       for_pretrain=True)
     # load from checkpoint
     if pretrained and args.load_ckpt:
         raise UserWarning('Both pretrained and load_ckpt are set. Do you intend to load from '
@@ -114,7 +97,7 @@ def get_model(args, ctx):
     model.cast(args.dtype)
     # TODO skip init?
     model.initialize(init=mx.init.Normal(0.02), ctx=ctx)
-    model.hybridize(static_alloc=True)
+    #model.hybridize(static_alloc=True)
     logging.debug(model)
 
     # losses
@@ -123,103 +106,56 @@ def get_model(args, ctx):
     nsp_loss.hybridize(static_alloc=True, static_shape=True)
     mlm_loss.hybridize(static_alloc=True, static_shape=True)
 
-    return model, nsp_loss, mlm_loss
+    return model, nsp_loss, mlm_loss, vocabulary
 
-ctx = [mx.gpu(i) for i in range(args.num_gpus)]
-model, nsp_loss, mlm_loss = get_model(args, ctx)
-metric = mx.metric.Accuracy()
+def get_dataset(data):
+    mx.nd.waitall()
+    t0 = time.time()
+    data_train = NumpyDataset(data)
+    t1 = time.time()
+    logging.info('Loading {} took {:.3f}s'.format(data, t1-t0))
+    logging.info(data_train.keys)
+    return data_train
 
-def get_dataset(data, data_eval):
+def get_dataloader(data):
+    dataloader = DataLoader(data, batch_size=args.batch_size,
+                                 shuffle=True, last_batch='rollover')
+    # prefetch iter
+    # preload to GPU
+    return dataloader
 
-    # stream of stream?
-    train_data = SimpleDatasetStream(cls, file_pattern, 'random', kwargs)
-
-    class BERTDataset(ArrayDataset):
-        def __init__(self, path):
-            self._path = path
-            dataset = np.load(path)
-            self._next_sentence_labels = dataset["next_sentence_labels"]
-            self._input_ids = dataset["input_ids"]
-            self._segment_ids = dataset["segment_ids"]
-            self._masked_lm_positions = dataset["masked_lm_positions"]
-            self._masked_lm_ids = dataset["masked_lm_ids"]
-            self._masked_lm_weights = dataset["masked_lm_weights"]
-            self._valid_lens = dataset["valid_lengths"]
-            arr = [self._next_sentence_labels, self._input_ids, self._segment_ids, self._masked_lm_positions, self._masked_lm_ids, self._masked_lm_weights, self._valid_lens]
-            super(BERTDataset, self).__init__(*arr)
-
-    file_path = '/tmp/tf_examples.npz'
-    data_train = BERTDataset(file_path)
-    data_eval = BERTDataset(file_path)
-    return data_train, data_eval
-
-def get_dataloader(data, data_eval):
-    #trans = ClassificationTransform(tokenizer, MRPCDataset.get_labels(), args.max_len)
-    #data_train = MRPCDataset('train').transform(trans)
-    #data_dev = MRPCDataset('dev').transform(trans)
-
-    bert_dataloader = DataLoader(data_train, batch_size=batch_size,
-                                   shuffle=True, last_batch='rollover')
-    bert_dataloader_dev = DataLoader(data_dev, batch_size=test_batch_size,
-                                                   shuffle=False)
-
-    return dataloader_train, dataloader_eval
-
-batch_size = args.batch_size * args.num_gpus
-test_batch_size = args.test_batch_size * args.num_gpus
-
-do_lower_case = 'uncased' in dataset
-tokenizer = FullTokenizer(vocabulary, do_lower_case=do_lower_case)
-dataset_train, dataset_eval = get_dataset(args.data, args.data_eval)
-data_train, data_eval = get_dataloader(batch_size, test_batch_size)
-
-def evaluate():
-    """Evaluate the model on validation dataset.
-    """
-    step_loss = 0
-    metric.reset()
-    nsp_loss.hybridize(static_alloc=True)
-    mlm_loss.hybridize(static_alloc=True)
-    for _, seqs in enumerate(bert_dataloader_dev):
-        Ls = []
-        input_ids, valid_len, type_ids, label = seqs
-        out = model(input_ids.as_in_context(ctx), type_ids.as_in_context(ctx),
-                    valid_len.astype('float32').as_in_context(ctx))
-        ls = nsp_loss(out, label.as_in_context(ctx)).mean()
-        Ls.append(ls)
-        step_loss += sum([L.asscalar() for L in Ls])
-        metric.update([label], [out])
-    logging.info('validation accuracy: %s', metric.get()[1])
-
-def train():
+def train(data_train, model, nsp_loss, mlm_loss, vocab_size, ctx):
     """Training function."""
-    trainer = gluon.Trainer(model.collect_params(), 'bertadam',
-                            {'learning_rate': lr, 'epsilon': 1e-6, 'wd': 0.01},
-                            kvstore=args.kv)
+    mlm_metric = mx.metric.Accuracy()
+    nsp_metric = mx.metric.Accuracy()
+    mlm_metric.reset()
+    nsp_metric.reset()
 
-    num_train_examples = len(data_train)
-    print('num_train_examples=', num_train_examples)
-    num_train_steps = int(num_train_examples / batch_size * args.epochs)
+    lr = args.lr
+    trainer = gluon.Trainer(model.collect_params(), 'bertadam',
+                            {'learning_rate': lr, 'epsilon': 1e-6, 'wd': 0.01})
+    num_train_steps = args.num_steps
     warmup_ratio = args.warmup_ratio
     num_warmup_steps = int(num_train_steps * warmup_ratio)
-    step_num = 0
-    params = []
+    params = [p for p in model.collect_params().values() if p.grad_req != 'null']
 
     # Do not apply weight decay on LayerNorm and bias terms
     for _, v in model.collect_params('.*beta|.*gamma|.*bias').items():
         v.wd_mult = 0.0
 
-    for p in model.collect_params().values():
-        if p.grad_req != 'null':
-            params.append(p)
     mx.nd.waitall()
 
-    for epoch_id in range(args.epochs):
-        metric.reset()
-        step_loss = 0
-        t0 = time.time()
-        for batch_id, seqs in enumerate(bert_dataloader):
+    begin_time = time.time()
+    step_mlm_loss = 0
+    step_nsp_loss = 0
+    batch_num = 0
+    step_num = 0
+    while step_num < num_train_steps:
+        for _, data in enumerate(data_train):
+            if step_num >= num_train_steps:
+                break
             step_num += 1
+            # update learning rate
             if step_num < num_warmup_steps:
                 new_lr = lr * step_num / num_warmup_steps
             else:
@@ -227,67 +163,71 @@ def train():
                 new_lr = lr - offset
             trainer.set_learning_rate(new_lr)
 
-            def _split_load(arr, dtype, ctx):
-                return mx.gluon.utils.split_and_load(arr.astype(args.dtype, copy=False), ctx)
+            def as_in_ctx(arrs, ctx):
+                if isinstance(arrs, list):
+                    return [arr.as_in_context(ctx) for arr in arrs]
+                return arrs.as_in_context(ctx)
 
-            next_sentence_labels, input_ids, segment_ids, masked_lm_positions, masked_lm_ids, masked_lm_weights, valid_lens = seqs
-
-            next_sentence_labels = _split_load(next_sentence_labels, args.dtype, ctx)
-            input_ids = _split_load(input_ids, args.dtype, ctx)
-            segment_ids = _split_load(segment_ids, args.dtype, ctx)
-            masked_lm_positions = _split_load(masked_lm_positions, args.dtype, ctx)
-            masked_lm_ids = _split_load(masked_lm_ids, args.dtype, ctx)
-            masked_lm_weights = _split_load(masked_lm_weights, args.dtype, ctx)
-            valid_lens = _split_load(valid_lens, args.dtype, ctx)
-            Ls = []
-            classified_list = []
-
-            for x in zip(next_sentence_labels, input_ids, segment_ids, masked_lm_positions, masked_lm_ids, masked_lm_weights, valid_lens):
-                with mx.autograd.record():
-                    next_sentence_label, input_id, segment_id, masked_lm_position, masked_lm_id, masked_lm_weight, valid_len = x
-
-                    classified, decode_out = model(input_id, segment_id, valid_length=valid_len, positions=masked_lm_position)
-                    decode_out = decode_out.reshape((-1, len(vocabulary)))
-                    #position_id = position_id.reshape((-1,))
-                    ls = nsp_loss(classified, next_sentence_label)
-
-                    ls2 = mlm_loss(decode_out, masked_lm_id.reshape((-1))) * masked_lm_weight.reshape((-1))
-                    ls = ls.mean() + ls2.mean()
-                    Ls.append(ls)
-                    classified_list.append(classified)
-                mx.autograd.backward(ls)
-
-            grads = [p.grad(c) for p in params for c in ctx]
-            gluon.utils.clip_global_norm(grads, 1)
-            trainer.step(1)
-
-            for ls in Ls:
-                step_loss += ls.asscalar()
-            metric.update(next_sentence_labels, classified_list)
-            mx.nd.waitall()
-            if (batch_id + 1) % (args.log_interval) == 0:
-                if args.profile:
-                    if batch_id + 1 == args.log_interval:
-                        mx.nd.waitall()
-                        mx.profiler.set_config(profile_memory=False,profile_symbolic=True, aggregate_stats=True, profile_all=True, filename='profile_output.json')
-                        mx.profiler.set_state('run')
-                    elif batch_id + 1 == args.log_interval * 2:
-                        mx.nd.waitall()
-                        mx.profiler.set_state('stop')
-                        print(mx.profiler.dumps())
-                        exit()
-
-                t1 = time.time()
-                logging.info('[Epoch {} Batch {}/{}] loss={:.4f}, lr={:.7f}, acc={:.3f}'
-                             .format(epoch_id, batch_id + 1, len(bert_dataloader),
-                                     step_loss / args.log_interval,
-                                     trainer.learning_rate, metric.get()[1]))
-                logging.info('Throughput={:.2f} K tokens/sec'
-                             .format(args.log_interval * batch_size * args.max_len * 1.0 / (t1 - t0) / 1000))
-                t0 = t1
-                step_loss = 0
+            data = as_in_ctx(data, ctx[0])
+            with mx.autograd.record():
+                input_id, masked_lm_id, masked_lm_position, masked_lm_weight, next_sentence_label, segment_id, valid_length = data
+                valid_length = valid_length.astype('float32')
+                classified, decoded = model(input_id, segment_id, valid_length=valid_length, positions=masked_lm_position)
+                decoded = decoded.reshape((-1, vocab_size))
+                masked_lm_id = masked_lm_id.reshape((-1))
+                ls1 = mlm_loss(decoded, masked_lm_id) * masked_lm_weight.reshape((-1))
+                ls2 = nsp_loss(classified, next_sentence_label)
+                ls1 = ls1.mean()
+                ls2 = ls2.mean()
+                ls = ls1 + ls2
+            mx.autograd.backward(ls)
+            trainer.allreduce_grads()
+            clip_grad_global_norm(params, 1)
+            trainer.update(1)
+            step_mlm_loss += ls1.asscalar()
+            step_nsp_loss += ls2.asscalar()
+            nsp_metric.update(next_sentence_label, classified)
+            mlm_metric.update(masked_lm_id, decoded)
+            if (step_num + 1) % (args.log_interval) == 0:
+                #if args.profile:
+                #    if batch_id + 1 == args.log_interval:
+                #        mx.nd.waitall()
+                #        mx.profiler.set_config(profile_memory=False,profile_symbolic=True, aggregate_stats=True, profile_all=True, filename='profile_output.json')
+                #        mx.profiler.set_state('run')
+                #    elif batch_id + 1 == args.log_interval * 2:
+                #        mx.nd.waitall()
+                #        mx.profiler.set_state('stop')
+                #        print(mx.profiler.dumps())
+                #        exit()
+                end_time = time.time()
+                duration = end_time - begin_time
+                throughput = args.log_interval * batch_size * args.max_len / 1000.0 / duration
+                step_mlm_loss /= args.log_interval
+                step_nsp_loss /= args.log_interval
+                logging.info('[step {}]\tmlm_loss={:.3f}\tmlm_acc={:.1f}\tnsp_loss={:.3f}\tnsp_acc={:.1f}\tthroughput={:.1f}K tks/s\tlr={:.7f}'
+                             .format(step_num, step_mlm_loss, mlm_metric.get()[1] * 100, step_nsp_loss,
+                                     nsp_metric.get()[1] * 100, throughput, trainer.learning_rate))
+                begin_time = end_time
+                step_mlm_loss = 0
+                step_nsp_loss = 0
         mx.nd.waitall()
-        evaluate()
 
 if __name__ == '__main__':
-    train()
+    # random seed
+    seed = args.seed
+    np.random.seed(seed)
+    random.seed(seed)
+    mx.random.seed(seed)
+
+
+    ctx = [mx.gpu() if args.gpu else mx.cpu()]
+    model, nsp_loss, mlm_loss, vocabulary = get_model(ctx)
+
+    batch_size = args.batch_size * len(ctx)
+
+    do_lower_case = 'uncased' in args.dataset_name
+    tokenizer = FullTokenizer(vocabulary, do_lower_case=do_lower_case)
+    dataset_train = get_dataset(args.data)
+    data_train = get_dataloader(dataset_train)
+    if args.do_training:
+        train(data_train, model, nsp_loss, mlm_loss, len(tokenizer.vocab), ctx)
