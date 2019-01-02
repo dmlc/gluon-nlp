1b41bd2295f9b1dbdbb43131188cc7f80720c9e9
Ubuntu <ubuntu@ip-172-31-6-178.ec2.internal>
2019-01-02 02:27:28 +0000

    fix import error

--- tests/unittest/test_optimizer.py
+++ tests/unittest/test_optimizer.py
@@ -130,5 +130,9 @@ def test_bert_adam():
                     kwarg.update(cg_option)
                     kwarg.update(rg_option)
                     kwarg.update(wd_option)
-                    compare_optimizer(opt1(**kwarg), opt2(**kwarg), shape, dtype,
-                                      rtol=1e-4, atol=2e-5)
+                    try:
+                        compare_optimizer(opt1(**kwarg), opt2(**kwarg), shape, dtype,
+                                          rtol=1e-4, atol=2e-5)
+                    except ImportError:
+                        print('skipping test_bert_adam() because an old version of MXNet is found')
+                        return
