c24a0fed92450c11fbe83e34cc8d88b55d34ba29
Yimin Jiang <jymthu@gmail.com>
2019-01-10 11:30:26 +0800

    replace u' unicode

--- scripts/machine_translation/dataprocessor.py
+++ scripts/machine_translation/dataprocessor.py
@@ -263,6 +263,6 @@ def write_sentences(sentences, file_path):
     with io.open(file_path, 'w', encoding='utf-8') as of:
         for sent in sentences:
             if isinstance(sent, (list, tuple)):
-                of.write(unicode(' '.join(sent) + '\n'))
+                of.write(u' '.join(sent) + u'\n')
             else:
-                of.write(unicode(sent + '\n'))
+                of.write(u''.join(sent) + u'\n')
