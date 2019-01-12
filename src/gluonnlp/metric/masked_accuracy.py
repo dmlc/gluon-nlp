88f1bd07dd6fd9f0260c3dce9ad8c7470a72991c
Haibin Lin <linhaibin.eric@gmail.com>
2019-01-11 21:38:03 -0800

    Import method from mxnet

--- src/gluonnlp/metric/masked_accuracy.py
+++ src/gluonnlp/metric/masked_accuracy.py
@@ -20,43 +20,9 @@
 
 import mxnet as mx
 from mxnet import ndarray
+from mxnet.metric import check_label_shapes
 
 __all__ = ['EvalMetric', 'MaskedAccuracy']
-
-def check_label_shapes(labels, preds, wrap=False, shape=False):
-    """Helper function for checking shape of label and prediction
-
-    Parameters
-    ----------
-    labels : list of `NDArray`
-        The labels of the data.
-
-    preds : list of `NDArray`
-        Predicted values.
-
-    wrap : boolean
-        If True, wrap labels/preds in a list if they are single NDArray
-
-    shape : boolean
-        If True, check the shape of labels and preds;
-        Otherwise only check their length.
-    """
-    if not shape:
-        label_shape, pred_shape = len(labels), len(preds)
-    else:
-        label_shape, pred_shape = labels.shape, preds.shape
-
-    if label_shape != pred_shape:
-        raise ValueError("Shape of labels {} does not match shape of "
-                         "predictions {}".format(label_shape, pred_shape))
-
-    if wrap:
-        if isinstance(labels, ndarray.ndarray.NDArray):
-            labels = [labels]
-        if isinstance(preds, ndarray.ndarray.NDArray):
-            preds = [preds]
-
-    return labels, preds
 
 class EvalMetric(object):
     """Base class for all evaluation metrics.
