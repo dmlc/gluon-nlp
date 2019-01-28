9ff0387f40f6984faf465808f5cb8c4f0190ceb0
fiercex <fiercex@outlook.com>
2019-01-28 16:59:17 +0800

    fix bug

--- scripts/bert/finetune_squad.py
+++ scripts/bert/finetune_squad.py
@@ -333,10 +333,10 @@ def train():
 
     epoch_tic = time.time()
     total_num = 0
+    log_num = 0
     for epoch_id in range(epochs):
         step_loss = 0.0
-        tic = time.time()
-        log_num = 0
+        tic = time.time() 
         for batch_id, data in enumerate(train_dataloader):
             # set new lr
             step_num = set_new_lr(step_num, batch_id)
@@ -371,6 +371,7 @@ def train():
                                  trainer.learning_rate, toc - tic, log_num/(toc - tic)))
                 tic = time.time()
                 step_loss = 0.0
+                log_num = 0
         epoch_toc = time.time()
         log.info('Time cost={:.2f} s, Thoughput={:.2f} samples/s'.format(
             epoch_toc - epoch_tic, total_num/(epoch_toc - epoch_tic)))
