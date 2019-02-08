cb8e402955ccdd124bdb990e86c2b41ad9867293
Anurag Sarkar <45988360+anuragsarkar97@users.noreply.github.com>
2019-02-08 10:26:01 +0530

    Update sampler.py
    I am not sure about it. but should work fine.

--- src/gluonnlp/data/sampler.py
+++ src/gluonnlp/data/sampler.py
@@ -290,9 +290,9 @@ class FixedBucketSampler(Sampler):
     bucket_scheme : BucketScheme, default ConstWidthBucket
         It is used to generate bucket keys. It supports:
         ConstWidthBucket: all the buckets have the same width
-        LinearWidthBucket: the width of i^{th}  bucket follows :math:`w_i = \alpha * i + 1`
-        ExpWidthBucket: the width of i^{th} bucket follows
-        :math:`w_i = \texttt{\detokenize{bucket_len_step}} * w_{i-1}`
+        LinearWidthBucket: the width of :math: `i^{th}`  bucket follows :math:`w_i = \alpha * i + 1`
+        ExpWidthBucket: the width of :math: `i^{th}` bucket follows
+        :math:`w_i` = bucket_len_step :math:`* w_{i-1}`
     Examples
     --------
     >>> lengths = [np.random.randint(1, 100) for _ in range(1000)]
