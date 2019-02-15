4a0b9de93532f1ace2bd30d5b7f9d9de8d705d4c
Gary Lai <garylai1990@gmail.com>
2019-02-14 21:58:22 -0500

    :construction: (bert) py2 compatibility

--- scripts/bert/embedding.py
+++ scripts/bert/embedding.py
@@ -24,7 +24,7 @@ from gluonnlp.data import BERTTokenizer, BERTSentenceTransform
 from .dataset import BertEmbeddingDataset
 
 
-class BertEmbedding:
+class BertEmbedding(object):
     """
     Encoding from BERT model.
 
