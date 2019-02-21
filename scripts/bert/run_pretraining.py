557f20f9e290690e952846ffd2968179dbf24513
Ubuntu <ubuntu@ip-172-31-41-221.ec2.internal>
2019-02-21 22:49:50 +0000

    refactor forward_backward fucntion

--- scripts/bert/run_pretraining.py
+++ scripts/bert/run_pretraining.py
@@ -178,6 +178,26 @@ def split_and_load(arrs, ctx):
         loaded_arrs = [gluon.utils.split_and_load(arr, ctx, even_split=False) for arr in arrs]
         return zip(*loaded_arrs)
 
+def forward(data, model, mlm_loss, nsp_loss, vocab_size):
+    """forward computation for evaluation"""
+    (input_id, masked_id, masked_position, masked_weight, \
+     next_sentence_label, segment_id, valid_length) = data
+    num_masks = masked_weight.sum() + 1e-8
+    valid_length = valid_length.reshape(-1)
+    masked_id = masked_id.reshape(-1)
+    valid_length_typed = valid_length.astype(args.dtype, copy=False)
+    _, _, classified, decoded = model(input_id, segment_id, valid_length_typed,
+                                      masked_position)
+    decoded = decoded.reshape((-1, vocab_size))
+    ls1 = mlm_loss(decoded.astype('float32', copy=False),
+                   masked_id, masked_weight.reshape((-1, 1)))
+    ls2 = nsp_loss(classified.astype('float32', copy=False), next_sentence_label)
+    ls1 = ls1.sum() / num_masks
+    ls2 = ls2.mean()
+    ls = ls1 + ls2
+    return ls, next_sentence_label, classified, masked_id, decoded, \
+           masked_weight, ls1, ls2, valid_length.astype('float32', copy=False)
+
 class ParallelBERT(Parallelizable):
     """Data parallel BERT model.
 
@@ -196,22 +216,10 @@ class ParallelBERT(Parallelizable):
 
     def forward_backward(self, x):
         """forward backward implementation"""
-        (input_id, masked_id, masked_position, masked_weight,
-         next_sentence_label, segment_id, valid_length) = x
-        num_masks = masked_weight.sum() + 1e-8
-        valid_length = valid_length.reshape(-1)
-        masked_id = masked_id.reshape(-1)
         with mx.autograd.record():
-            valid_length_typed = valid_length.astype(args.dtype, copy=False)
-            _, _, classified, decoded = self._model(input_id, segment_id, valid_length_typed,
-                                                    masked_position)
-            decoded = decoded.reshape((-1, self._vocab_size))
-            ls1 = self._mlm_loss(decoded.astype('float32', copy=False),
-                                 masked_id, masked_weight.reshape((-1, 1)))
-            ls2 = self._nsp_loss(classified.astype('float32', copy=False), next_sentence_label)
-            ls1 = ls1.sum() / num_masks
-            ls2 = ls2.mean()
-            ls = ls1 + ls2
+            (ls, next_sentence_label, classified, masked_id, decoded, \
+             masked_weight, ls1, ls2, valid_length) = forward(x, self._model, self._mlm_loss,
+                                                              self._nsp_loss, self._vocab_size)
             ls = ls / self._rescale_factor
         if args.dtype == 'float16':
             self._trainer.backward(ls)
@@ -219,22 +227,6 @@ class ParallelBERT(Parallelizable):
             ls.backward()
         return ls, next_sentence_label, classified, masked_id, decoded, \
                masked_weight, ls1, ls2, valid_length
-
-def forward(data, model, mlm_loss, nsp_loss, vocab_size):
-    """forward computation for evaluation"""
-    (input_id, masked_id, masked_position, masked_weight, \
-     next_sentence_label, segment_id, valid_length) = data
-    num_masks = masked_weight.sum() + 1e-8
-    valid_length = valid_length.reshape(-1)
-    valid_length = valid_length.astype('float32', copy=False)
-    _, _, classified, decoded = model(input_id, segment_id, valid_length, masked_position)
-    masked_id = masked_id.reshape(-1)
-    decoded = decoded.reshape((-1, vocab_size))
-    ls1 = mlm_loss(decoded, masked_id, masked_weight.reshape((-1, 1))).sum() / num_masks
-    ls2 = nsp_loss(classified, next_sentence_label).mean()
-    ls = ls1 + ls2
-    return ls, next_sentence_label, classified, masked_id, decoded, \
-           masked_weight, ls1, ls2, valid_length
 
 def evaluate(data_eval, model, nsp_loss, mlm_loss, vocab_size, ctx):
     """Evaluation function."""
