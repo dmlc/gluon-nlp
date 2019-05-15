55ca9d4c8ea91e3f1d390b5bde5c9ec6bd16b325
Haibin Lin <linhaibin.eric@gmail.com>
2019-05-15 15:21:57 -0700

    [Enhancement] runtime training sample generation for BERT (#687)
    * refactor create_pretaining
    
    * operate in integer space
    
    * fix bug
    
    * fix bug
    
    * online dataset
    
    * add raw option
    
    * add test, and option to hvd script
    
    * remove prefer int
    
    * remove int inputs option
    
    * fix lint
    
    * refactor test
    
    * remove duplicate test
    
    * address comments
    
    * fix lint
    
    * mark serial
    
    * fix lint
    
    * Update test_question_answering.py
    
    * multiprocessing support
    
    * fix lint
    
    * properly join and terminate
    
    * remove super.__del__
    
    * revert daemon changes
    
    * revert num worker change
    
    * disable test

--- scripts/bert/pretraining_utils.py
+++ scripts/bert/pretraining_utils.py
@@ -21,18 +21,20 @@
 import glob
 import time
 import os
+import functools
 import logging
 import argparse
 import mxnet as mx
 from mxnet.gluon.data import DataLoader
+from create_pretraining_data import create_training_instances
 
 import gluonnlp as nlp
 from gluonnlp.data.batchify import Tuple, Stack, Pad
 from gluonnlp.metric import MaskedAccuracy
 
-__all__ = ['get_model_loss', 'get_pretrain_dataset', 'get_dummy_dataloader',
+__all__ = ['get_model_loss', 'get_pretrain_data_npz', 'get_dummy_dataloader',
            'save_parameters', 'save_states', 'evaluate', 'forward', 'split_and_load',
-           'get_argparser']
+           'get_argparser', 'get_pretrain_data_text']
 
 def get_model_loss(ctx, model, pretrained, dataset_name, dtype, ckpt_dir=None, start_step=None):
     """Get model for pre-training."""
@@ -60,9 +62,121 @@ def get_model_loss(ctx, model, pretrained, dataset_name, dtype, ckpt_dir=None, s
 
     return model, nsp_loss, mlm_loss, vocabulary
 
-def get_pretrain_dataset(data, batch_size, num_ctxes, shuffle, use_avg_len,
-                         num_buckets, num_parts=1, part_idx=0, prefetch=True):
-    """create dataset for pretraining."""
+class BERTPretrainDataset(mx.gluon.data.ArrayDataset):
+    """Dataset for BERT pre-training.
+
+    Each record contains the following numpy ndarrays: input_ids, masked_lm_ids,
+    masked_lm_positions, masked_lm_weights, next_sentence_labels, segment_ids, valid_lengths.
+
+    Parameters
+    ----------
+    filename : str
+        Path to the input text file.
+    tokenizer : BERTTokenizer
+        The BERTTokenizer
+    max_seq_length : int
+        The hard limit of maximum sequence length of sentence pairs
+    short_seq_prob : float
+        The probability of sampling sequences shorter than the max_seq_length.
+    masked_lm_prob : float
+        The probability of replacing texts with masks/random words/original words.
+    max_predictions_per_seq : int
+        The hard limit of the number of predictions for masked words
+    vocab : BERTVocab
+        The BERTVocab
+    """
+    def __init__(self, filename, tokenizer, max_seq_length, short_seq_prob,
+                 masked_lm_prob, max_predictions_per_seq, vocab):
+        logging.debug('start to load file %s ...', filename)
+        instances = create_training_instances([filename], tokenizer, max_seq_length,
+                                              short_seq_prob, masked_lm_prob,
+                                              max_predictions_per_seq, vocab,
+                                              nworker=1)
+        super(BERTPretrainDataset, self).__init__(*instances)
+
+def get_pretrain_data_text(data, batch_size, num_ctxes, shuffle, use_avg_len,
+                           num_buckets, vocab, max_seq_length, short_seq_prob,
+                           masked_lm_prob, max_predictions_per_seq,
+                           cased, num_parts=1, part_idx=0,
+                           prefetch=True):
+    """create dataset for pretraining based on raw texts"""
+    num_files = len(glob.glob(os.path.expanduser(data)))
+    logging.debug('%d files found.', num_files)
+    assert num_files >= num_parts, \
+        'Number of training files must be greater than the number of partitions'
+
+    tokenizer = nlp.data.BERTTokenizer(vocab=vocab, lower=not cased)
+    dataset_cls = functools.partial(BERTPretrainDataset, tokenizer=tokenizer,
+                                    max_seq_length=max_seq_length,
+                                    short_seq_prob=short_seq_prob,
+                                    masked_lm_prob=masked_lm_prob,
+                                    max_predictions_per_seq=max_predictions_per_seq,
+                                    vocab=vocab)
+
+    split_sampler = nlp.data.SplitSampler(num_files, num_parts=num_parts, part_index=part_idx)
+    stream = nlp.data.SimpleDatasetStream(dataset_cls, data, split_sampler)
+    if prefetch:
+        stream = nlp.data.PrefetchingStream(stream, worker_type='process')
+    # create data loader based on the dataset
+    dataloader_xform = BERTLoaderTransform(use_avg_len, batch_size,
+                                           shuffle, num_ctxes, num_buckets)
+    stream = stream.transform(dataloader_xform)
+    return stream
+
+class BERTLoaderTransform(object):
+    """Create dataloader for a BERT dataset. """
+
+    def __init__(self, use_avg_len, batch_size, shuffle, num_ctxes, num_buckets):
+        self._use_avg_len = use_avg_len
+        self._batch_size = batch_size
+        self._shuffle = shuffle
+        self._num_ctxes = num_ctxes
+        self._num_buckets = num_buckets
+
+    def __call__(self, dataset):
+        """create data loader based on the dataset chunk"""
+        if isinstance(dataset, nlp.data.NumpyDataset):
+            lengths = dataset.get_field('valid_lengths')
+        elif isinstance(dataset, BERTPretrainDataset):
+            lengths = dataset.transform(lambda input_ids, segment_ids, masked_lm_positions, \
+                                               masked_lm_ids, masked_lm_weights, \
+                                               next_sentence_labels, valid_lengths: \
+                                               valid_lengths, lazy=False)
+        else:
+            raise ValueError('unexpected dataset type: %s'%str(dataset))
+
+        # A batch includes: input_id, masked_id, masked_position, masked_weight,
+        #                   next_sentence_label, segment_id, valid_length
+        batchify_fn = Tuple(Pad(), Pad(), Pad(), Pad(), Stack(), Pad(), Stack())
+        if self._use_avg_len:
+            # sharded data loader
+            sampler = nlp.data.FixedBucketSampler(lengths=lengths,
+                                                  # batch_size per shard
+                                                  batch_size=self._batch_size,
+                                                  num_buckets=self._num_buckets,
+                                                  shuffle=self._shuffle,
+                                                  use_average_length=True,
+                                                  num_shards=self._num_ctxes)
+            dataloader = nlp.data.ShardedDataLoader(dataset,
+                                                    batch_sampler=sampler,
+                                                    batchify_fn=batchify_fn,
+                                                    num_workers=self._num_ctxes)
+        else:
+            sampler = nlp.data.FixedBucketSampler(lengths,
+                                                  batch_size=self._batch_size * self._num_ctxes,
+                                                  num_buckets=self._num_buckets,
+                                                  ratio=0,
+                                                  shuffle=self._shuffle)
+            dataloader = DataLoader(dataset=dataset,
+                                    batch_sampler=sampler,
+                                    batchify_fn=batchify_fn,
+                                    num_workers=1)
+        logging.debug('Sampler created for a new dataset:\n%s', sampler.stats())
+        return dataloader
+
+def get_pretrain_data_npz(data, batch_size, num_ctxes, shuffle, use_avg_len,
+                          num_buckets, num_parts=1, part_idx=0, prefetch=True):
+    """create dataset for pretraining based on pre-processed npz files."""
     num_files = len(glob.glob(os.path.expanduser(data)))
     logging.debug('%d files found.', num_files)
     assert num_files >= num_parts, \
@@ -72,39 +186,10 @@ def get_pretrain_dataset(data, batch_size, num_ctxes, shuffle, use_avg_len,
     if prefetch:
         stream = nlp.data.PrefetchingStream(stream)
 
-    def get_dataloader(dataset):
-        """create data loader based on the dataset chunk"""
-        lengths = dataset.get_field('valid_lengths')
-        # A batch includes: input_id, masked_id, masked_position, masked_weight,
-        #                   next_sentence_label, segment_id, valid_length
-        batchify_fn = Tuple(Pad(), Pad(), Pad(), Pad(), Stack(), Pad(), Stack())
-        if use_avg_len:
-            # sharded data loader
-            sampler = nlp.data.FixedBucketSampler(lengths=lengths,
-                                                  # batch_size per shard
-                                                  batch_size=batch_size,
-                                                  num_buckets=num_buckets,
-                                                  shuffle=shuffle,
-                                                  use_average_length=True,
-                                                  num_shards=num_ctxes)
-            dataloader = nlp.data.ShardedDataLoader(dataset,
-                                                    batch_sampler=sampler,
-                                                    batchify_fn=batchify_fn,
-                                                    num_workers=num_ctxes)
-        else:
-            sampler = nlp.data.FixedBucketSampler(lengths,
-                                                  batch_size=batch_size * num_ctxes,
-                                                  num_buckets=num_buckets,
-                                                  ratio=0,
-                                                  shuffle=shuffle)
-            dataloader = DataLoader(dataset=dataset,
-                                    batch_sampler=sampler,
-                                    batchify_fn=batchify_fn,
-                                    num_workers=1)
-        logging.debug('Sampler created for a new dataset:\n%s', sampler.stats())
-        return dataloader
-
-    stream = stream.transform(get_dataloader)
+    # create data loader based on the dataset
+    dataloader_xform = BERTLoaderTransform(use_avg_len, batch_size,
+                                           shuffle, num_ctxes, num_buckets)
+    stream = stream.transform(dataloader_xform)
     return stream
 
 def get_dummy_dataloader(dataloader, target_shape):
--- scripts/tests/test_scripts.py
+++ scripts/tests/test_scripts.py
@@ -205,45 +205,30 @@ def test_pretrain():
     try:
         # TODO(haibin) update test once MXNet 1.5 is released.
         from mxnet.ndarray.contrib import adamw_update
+        arguments = ['--log_interval', '2', '--data_eval', './test/bert/data/*.npz',
+                     '--batch_size_eval', '8', '--ckpt_dir', './test/bert/ckpt', '--gpus', '0',
+                     '--num_steps', '20']
         # test training
         process = subprocess.check_call([sys.executable, './scripts/bert/run_pretraining.py',
-                                         '--gpus', '0',
                                          '--data', './test/bert/data/*.npz',
                                          '--batch_size', '32',
                                          '--lr', '2e-5',
                                          '--warmup_ratio', '0.5',
-                                         '--num_steps', '20',
-                                         '--pretrained',
-                                         '--log_interval', '2',
-                                         '--data_eval', './test/bert/data/*.npz',
-                                         '--batch_size_eval', '8',
-                                         '--ckpt_dir', './test/bert/ckpt'])
+                                         '--pretrained'] + arguments)
         # test evaluation
         process = subprocess.check_call([sys.executable, './scripts/bert/run_pretraining.py',
-                                         '--gpus', '0',
-                                         '--num_steps', '20',
-                                         '--pretrained',
-                                         '--log_interval', '2',
-                                         '--data_eval', './test/bert/data/*.npz',
-                                         '--batch_size_eval', '8',
-                                         '--ckpt_dir', './test/bert/ckpt'])
+                                         '--pretrained'] + arguments)
 
         # test mixed precision training and use-avg-len
         from mxnet.ndarray.contrib import mp_adamw_update
         process = subprocess.check_call([sys.executable, './scripts/bert/run_pretraining.py',
-                                         '--gpus', '0',
                                          '--dtype', 'float16',
                                          '--data', './test/bert/data/*.npz',
                                          '--batch_size', '4096',
                                          '--use_avg_len',
                                          '--lr', '2e-5',
                                          '--warmup_ratio', '0.5',
-                                         '--num_steps', '20',
-                                         '--pretrained',
-                                         '--log_interval', '2',
-                                         '--data_eval', './test/bert/data/*.npz',
-                                         '--batch_size_eval', '8',
-                                         '--ckpt_dir', './test/bert/ckpt'])
+                                         '--pretrained'] + arguments)
         time.sleep(5)
     except ImportError:
         print("The test expects master branch of MXNet. Skipped now.")
@@ -269,26 +254,31 @@ def test_pretrain_hvd():
         # TODO(haibin) update test once MXNet 1.5 is released.
         from mxnet.ndarray.contrib import adamw_update
         import horovod.mxnet as hvd
+        arguments = ['--log_interval', '2', '--data_eval', './test/bert/data/*.npz',
+                     '--batch_size_eval', '8', '--ckpt_dir', './test/bert/ckpt',
+                     '--num_steps', '20']
         # test training
         process = subprocess.check_call([sys.executable, './scripts/bert/run_pretraining_hvd.py',
                                          '--data', './test/bert/data/*.npz',
                                          '--batch_size', '32',
                                          '--lr', '2e-5',
                                          '--warmup_ratio', '0.5',
-                                         '--num_steps', '20',
-                                         '--pretrained',
-                                         '--log_interval', '2',
-                                         '--data_eval', './test/bert/data/*.npz',
-                                         '--batch_size_eval', '8',
-                                         '--ckpt_dir', './test/bert/ckpt'])
+                                         '--pretrained'] + arguments)
+        # test training with raw data
+        process = subprocess.check_call([sys.executable, './scripts/bert/run_pretraining_hvd.py',
+                                         '--raw',
+                                         '--max_seq_length', '128',
+                                         '--max_predictions_per_seq', '20',
+                                         '--masked_lm_prob', '0.15',
+                                         '--short_seq_prob', '0.1',
+                                         '--data', './scripts/bert/sample_text.txt',
+                                         '--batch_size', '32',
+                                         '--lr', '2e-5',
+                                         '--warmup_ratio', '0.5',
+                                         '--pretrained'] + arguments)
         # test evaluation
         process = subprocess.check_call([sys.executable, './scripts/bert/run_pretraining_hvd.py',
-                                         '--num_steps', '20',
-                                         '--pretrained',
-                                         '--log_interval', '2',
-                                         '--data_eval', './test/bert/data/*.npz',
-                                         '--batch_size_eval', '8',
-                                         '--ckpt_dir', './test/bert/ckpt'])
+                                         '--pretrained'] + arguments)
 
         # test mixed precision training and use-avg-len
         from mxnet.ndarray.contrib import mp_adamw_update
@@ -299,12 +289,7 @@ def test_pretrain_hvd():
                                          '--use_avg_len',
                                          '--lr', '2e-5',
                                          '--warmup_ratio', '0.5',
-                                         '--num_steps', '20',
-                                         '--pretrained',
-                                         '--log_interval', '2',
-                                         '--data_eval', './test/bert/data/*.npz',
-                                         '--batch_size_eval', '8',
-                                         '--ckpt_dir', './test/bert/ckpt'])
+                                         '--pretrained'] + arguments)
         time.sleep(5)
     except ImportError:
         print("The test expects master branch of MXNet and Horovod. Skipped now.")
