0dc73fb69c0ace238419cb6bab20efc5315968d0
haven-jeon <gogamza@gmail.com>
2019-05-19 23:02:18 +0900

    fix flake8 error

--- src/gluonnlp/data/utils.py
+++ src/gluonnlp/data/utils.py
@@ -432,7 +432,7 @@ def convert_to_unicode(text):
     elif six.PY2:
         if isinstance(text, str):
             return text.decode('utf-8', 'ignore')
-        elif isinstance(text, unicode):  # pylint: disable=disable=E0602, E0603
+        elif isinstance(text, unicode):  # noqa: F821
             return text
         else:
             raise ValueError('Unsupported string type: %s' % (type(text)))
