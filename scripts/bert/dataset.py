091470b34511344417651b7a674ab7471c27c88c
paperplanet <zyn2122@gmail.com>
2019-05-27 11:05:58 +0800

    try to fix baidu_ernie_data import issue

--- scripts/bert/dataset.py
+++ scripts/bert/dataset.py
@@ -29,7 +29,10 @@ from mxnet.gluon.data import Dataset
 from gluonnlp.data import TSVDataset, BERTSentenceTransform, GlueCoLA, GlueSST2, GlueSTSB
 from gluonnlp.data import GlueQQP, GlueRTE, GlueMNLI, GlueQNLI, GlueWNLI
 from gluonnlp.data.registry import register
-from .baidu_ernie_data import BaiduErnieXNLI
+try:
+    from .baidu_ernie_data import BaiduErnieXNLI
+except:
+    from baidu_ernie_data import BaiduErnieXNLI
 
 @register(segment=['train', 'dev', 'test'])
 class MRPCDataset(TSVDataset):
