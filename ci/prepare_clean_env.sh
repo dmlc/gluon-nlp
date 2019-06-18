eaf378b606a03893ab5459a6ff42dc2bf387038a
Sheng Zha <szha@users.noreply.github.com>
2019-06-18 15:49:55 -0700

    Update prepare_clean_env.sh

--- ci/prepare_clean_env.sh
+++ ci/prepare_clean_env.sh
@@ -6,6 +6,9 @@ export CUDA_VISIBLE_DEVICES=$EXECUTOR_NUMBER
 export CONDA_ENVS_PATH=$PWD/conda
 export CONDA_PKGS_DIRS=$PWD/conda/pkgs
 export MXNET_HOME=$PWD/tests/data
+export HOROVOD_WITHOUT_TENSORFLOW=1
+export HOROVOD_WITHOUT_PYTORCH=1
+export HOROVOD_WITH_MXNET=1
 
 make clean
 conda env update --prune -p conda/${env_name} -f env/${env_name}.yml
@@ -17,9 +20,4 @@ pip install -v -e .
 python -m spacy download en
 python -m spacy download de
 python -m nltk.downloader all
-
-export HOROVOD_WITHOUT_TENSORFLOW=1
-export HOROVOD_WITHOUT_PYTORCH=1
-export HOROVOD_WITH_MXNET=1
-export LD_LIBRARY_PATH=/usr/local/cuda-9.2/lib64:$LD_LIBRARY_PATH
-pip install horovod==0.16.2 --no-cache-dir -U
+python -m pip install horovod==0.16.2 --no-cache-dir -U
