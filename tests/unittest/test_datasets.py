adb496ac3930d8c3547241b0f99d7c6f171a3641
Leonard Lausen <lausen@amazon.com>
2019-07-04 22:39:18 +0200

    [DOC] Fix sphinx-autorun errors on website (#807)
    * Remove unneeded warnings.filterwarnings calls from docstrings
    
    * Remove "-etc-" from runblocks in docstrings
    
    * Use Ellipsis
    
    * Remove sphinx-autorun and include full output in docstring
    
    * Re-enable SimVerb3500 test

--- tests/unittest/test_datasets.py
+++ tests/unittest/test_datasets.py
@@ -294,8 +294,6 @@ def test_simlex999():
 @flaky(max_runs=2, min_passes=1)
 @pytest.mark.serial
 @pytest.mark.remote_required
-@pytest.mark.skipif(datetime.date.today() < datetime.date(2019, 7, 18),
-                    reason='Disabled for 4 weeks due to link move.')
 def test_simverb3500():
     data = nlp.data.SimVerb3500(
         root=os.path.join('tests', 'externaldata', 'simverb3500'))
