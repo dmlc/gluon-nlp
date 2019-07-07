d02e8aa40e94a427dea24602030dc1e845ae6e8c
sxjscience <sxjscience001@gmail.com>
2019-07-06 20:58:35 -0700

    fix lint

--- scripts/bert/finetune_icsl.py
+++ scripts/bert/finetune_icsl.py
@@ -35,12 +35,12 @@ import random
 import numpy as np
 import pandas as pd
 from tqdm import tqdm
+from seqeval.metrics import f1_score as ner_f1_score
 import mxnet as mx
 from mxnet import gluon
 from mxnet.gluon import nn, Block
 import gluonnlp as nlp
 from gluonnlp.data import BERTTokenizer, ATISDataset, SNIPSDataset
-from seqeval.metrics import f1_score as ner_f1_score
 
 
 
