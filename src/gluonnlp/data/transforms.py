22041982b3dc1f6a0a0b96df5a460448efdb7cd0
Haibin Lin <linhaibin.eric@gmail.com>
2019-07-19 09:23:21 -0700

    Fix BERT Japanese Tokenization

--- src/gluonnlp/data/transforms.py
+++ src/gluonnlp/data/transforms.py
@@ -876,22 +876,28 @@ class BERTBasicTokenizer(object):
         return ''.join(output)
 
     def _is_chinese_char(self, cp):
-        """Checks whether CP is the codepoint of a CJK character."""
-        # This defines a "chinese character" as anything in the CJK Unicode block:
+        """Checks whether CP is the codepoint of a CJK, Katakana and Hiragana character."""
+        # This defines a "chinese character" as anything in the CJK, Katakana and Hiragana
+        # Unicode block
+        # 
         #   https://en.wikipedia.org/wiki/CJK_Unified_Ideographs_(Unicode_block)
+        #   https://en.wikipedia.org/wiki/Katakana_(Unicode_block)
+        #   https://en.wikipedia.org/wiki/Hiragana_(Unicode_block)        
         #
-        # Note that the CJK Unicode block is NOT all Japanese and Korean characters,
-        # despite its name. The modern Korean Hangul alphabet is a different block,
-        # as is Japanese Hiragana and Katakana. Those alphabets are used to write
-        # space-separated words, so they are not treated specially and handled
-        # like the all of the other languages.
+        # Note that the CJK Unicode block is NOT all Korean characters,
+        # despite its name. The modern Korean Hangul alphabet is a different block.
+        # Those alphabets are used to write space-separated words,
+        # so they are not treated specially and handled like the all of the other
+        # languages.
         if ((cp >= 0x4E00 and cp <= 0x9FFF) or (cp >= 0x3400 and cp <= 0x4DBF)
                 or (cp >= 0x20000 and cp <= 0x2A6DF)
                 or (cp >= 0x2A700 and cp <= 0x2B73F)
                 or (cp >= 0x2B740 and cp <= 0x2B81F)
                 or (cp >= 0x2B820 and cp <= 0x2CEAF)
                 or (cp >= 0xF900 and cp <= 0xFAFF)
-                or (cp >= 0x2F800 and cp <= 0x2FA1F)):
+                or (cp >= 0x2F800 and cp <= 0x2FA1F)
+                or (cp >= 0x30A0 and cp <= 0x30FF)
+                or (cp >= 0x3040 and cp <= 0x309F)):
             return True
 
         return False
