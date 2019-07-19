4f6c57a8df459f7ab9356ba714ae6c4d4585f783
Xiangkun Hu <xiangkhu@amazon.com>
2019-07-19 14:47:07 +0800

    Add BoolQ, ReCoRD, AX-g and AX-b dataset

--- src/gluonnlp/data/super_glue.py
+++ src/gluonnlp/data/super_glue.py
@@ -240,3 +240,92 @@ class SuperGlueMultiRC(_SuperGlueDataset):
 
     def _repo_dir(self):
         return 'gluon/dataset/SUPERGLUE/MultiRC'
+
+
+@register(segment=['train', 'val', 'test'])
+class SuperGlueBoolQ(_SuperGlueDataset):
+    def __init__(self, segment='train',
+                 root=os.path.join(get_home_dir(), 'datasets', 'superglue_boolq')):
+        self._segment = segment
+        self._data_file = {'train': ('train', '89507ff3015c3212b72318fb932cfb6d4e8417ef',
+                                     'd5be523290f49fc0f21f4375900451fb803817c0'),
+                           'val': ('val', 'fd39562fc2c9d0b2b8289d02a8cf82aa151d0ad4',
+                                   '9b09ece2b1974e4da20f0173454ba82ff8ee1710'),
+                           'test': ('test', 'a805d4bd03112366d548473a6848601c042667d3',
+                                    '98c308620c6d6c0768ba093858c92e5a5550ce9b')}
+        data_file = self._data_file[segment]
+
+        super(SuperGlueBoolQ, self).__init__(root, data_file)
+
+    def read_samples(self, samples):
+        for i, sample in enumerate(samples):
+            passage = sample['passage']
+            question = sample['question']
+            if self._segment == 'test':
+                samples[i] = [passage, question]
+            else:
+                label = int(sample['label'])
+                samples[i] = [passage, question, label]
+
+        return samples
+
+    def _repo_dir(self):
+        return 'gluon/dataset/SUPERGLUE/BoolQ'
+
+
+@register(segment=['train', 'val', 'test'])
+class SuperGlueReCoRD(_SuperGlueDataset):
+    def __init__(self, segment='train',
+                 root=os.path.join(get_home_dir(), 'datasets', 'superglue_record')):
+        self._segment = segment
+        self._data_file = {'train': ('train', '047282c912535c9a3bcea519935fde882feb619d',
+                                     '65592074cefde2ecd1b27ce7b35eb8beb86c691a'),
+                           'val': ('val', '442d8470bff2c9295231cd10262a7abf401edc64',
+                                   '9d1850e4dfe2eca3b71bfea191d5f4b412c65309'),
+                           'test': ('test', 'fc639a18fa87befdc52f14c1092fb40475bf50d0',
+                                    'b79b22f54b5a49f98fecd05751b122ccc6947c81')}
+        data_file = self._data_file[segment]
+
+        super(SuperGlueReCoRD, self).__init__(root, data_file)
+
+    def read_samples(self, samples):
+        for i, sample in enumerate(samples):
+            text = sample['passage']['text']
+            entities = sample['passage']['entities']
+            qas = sample['qas']
+            samples[i] = {'text': text, 'entities': entities, 'qas': qas}
+
+        return samples
+
+    def _repo_dir(self):
+        return 'gluon/dataset/SUPERGLUE/ReCoRD'
+
+
+@register(segment=['AX-b'])
+class SuperGlueAX_b(_SuperGlueDataset):
+    def __init__(self, root=os.path.join(get_home_dir(), 'datasets', 'superglue_ax_b')):
+        data_file = ('AX-g', '398c5a376eb436f790723cd217ac040334140000',
+                     '50fd8ac409897b652daa4b246917097c3c394bc8')
+
+        super(SuperGlueAX_b, self).__init__(root, data_file)
+
+    def read_samples(self, samples):
+        return samples
+
+    def _repo_dir(self):
+        return 'gluon/dataset/SUPERGLUE/AX-b'
+
+
+@register(segment=['AX-g'])
+class SuperGlueAX_g(_SuperGlueDataset):
+    def __init__(self, root=os.path.join(get_home_dir(), 'datasets', 'superglue_ax_g')):
+        data_file = ('AX-g', 'd8c92498496854807dfeacd344eddf466d7f468a',
+                     '8a8cbfe00fd88776a2a2f20b477e5b0c6cc8ebae')
+
+        super(SuperGlueAX_g, self).__init__(root, data_file)
+
+    def read_samples(self, samples):
+        return samples
+
+    def _repo_dir(self):
+        return 'gluon/dataset/SUPERGLUE/AX-g'
