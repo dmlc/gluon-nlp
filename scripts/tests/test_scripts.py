6fed9a6a50b88380edb54be04f65a7ca6bfed159
paperplanet <zyn2122@gmail.com>
2019-08-15 17:45:37 +0800

    delete lcqmc test

--- scripts/tests/test_scripts.py
+++ scripts/tests/test_scripts.py
@@ -359,7 +359,7 @@ def test_finetune_inference(dataset):
 @pytest.mark.gpu
 @pytest.mark.remote_required
 @pytest.mark.integration
-@pytest.mark.parametrize('dataset', ['XNLI', 'LCQMC', 'ChnSentiCorp'])
+@pytest.mark.parametrize('dataset', ['XNLI', 'ChnSentiCorp'])
 def test_finetune_chinese_inference(dataset):
     arguments = ['--log_interval', '100', '--epsilon', '1e-8', '--optimizer',
                  'adam', '--gpu', '0', '--max_len', '80', '--only_inference']
