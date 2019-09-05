332914aca991c883a100232b9c925d8480130f76
Haibin Lin <linhaibin.eric@gmail.com>
2019-09-05 00:00:28 -0700

    fix lint

--- src/gluonnlp/_pyversion.py
+++ src/gluonnlp/_pyversion.py
@@ -21,7 +21,6 @@
 import sys
 
 if not (sys.version_info[0] >= 3 and sys.version_info[1] >= 6):
-    PY3STATEMENT = """GluonNLP project proudly dropped support of Python2.
-    The minimal Python requirement is Python 3.6
-    """
+    PY3STATEMENT = 'GluonNLP project proudly dropped support of Python2.' \
+                   'The minimal Python requirement is Python 3.6'
     raise Exception(PY3STATEMENT)
