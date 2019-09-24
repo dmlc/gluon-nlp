f09de1286c0afc2c309406a1ca77f9da553cc280
Ubuntu <ubuntu@ip-172-31-36-64.ec2.internal>
2019-09-24 01:31:24 +0000

    add hvd dependency

--- ci/prepare_clean_env.sh
+++ ci/prepare_clean_env.sh
@@ -6,6 +6,9 @@ export CUDA_VISIBLE_DEVICES=$EXECUTOR_NUMBER
 export CONDA_ENVS_PATH=$PWD/conda
 export CONDA_PKGS_DIRS=$PWD/conda/pkgs
 export MXNET_HOME=$PWD/tests/data
+export HOROVOD_WITHOUT_TENSORFLOW=1
+export HOROVOD_WITHOUT_PYTORCH=1
+export HOROVOD_WITH_MXNET=1
 
 make clean
 conda env update --prune -p conda/${env_name} -f env/${env_name}.yml
@@ -14,6 +17,7 @@ conda list
 printenv
 
 pip install -v -e .
+pip install horovod --no-cache-dir -U
 python -m spacy download en
 python -m spacy download de
 python -m nltk.downloader all
