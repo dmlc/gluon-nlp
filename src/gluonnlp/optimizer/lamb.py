d3c913b72246e1cf49a5b97e90534fd117817dde
haibin lin <linhaibin.eric@gmail.com>
2019-09-24 20:50:45 +0000

    Revert "Revert "gRevert "revert lamb changes"""
    This reverts commit bb07f5f205d47cd502780ff258abac0dbbe24314.

--- src/gluonnlp/optimizer/lamb.py
+++ src/gluonnlp/optimizer/lamb.py
@@ -18,8 +18,8 @@
 """LAMB optimizer"""
 
 from mxnet.optimizer import Optimizer, register
-from mxnet.ndarray import zeros, NDArray
-from mxnet.ndarray import square, power, sqrt, maximum, minimum, clip
+from mxnet.ndarray import zeros, ones_like, NDArray
+from mxnet.ndarray import square, power, sqrt, maximum, minimum, clip, where
 
 __all__ = ['LAMB']
 
@@ -103,25 +103,34 @@ class LAMB(Optimizer):
             grad = clip(grad, -self.clip_gradient, self.clip_gradient)
 
         mean, var = state
-        mean[:] = self.beta1 * mean + (1. - self.beta1) * grad
-        var[:] = self.beta2 * var + (1. - self.beta2) * square(grad)
+        mean *= self.beta1
+        mean += (1. - self.beta1) * grad
+        var *= self.beta2
+        var += (1. - self.beta2) * square(grad)
 
         r1 = weight.norm()
         if not self.bias_correction:
             r1 = minimum(maximum(r1, self.lower_bound), self.upper_bound)
             g = mean / (sqrt(var) + self.epsilon) + wd * weight
-
         else:
-            # execution bias correction
+            # apply bias correction
             mean_hat = mean / (1. - power(self.beta1, t))
             var_hat = var / (1. - power(self.beta2, t))
-            g = mean_hat / sqrt(var_hat + self.epsilon) + wd * weight
+            var_hat += self.epsilon
+            var_hat[:] = sqrt(var_hat)
+            mean_hat /= var_hat
+            mean_hat += wd * weight
+            g = mean_hat
 
         r2 = g.norm()
 
         # calculate lamb_trust_ratio
-        r = 1. if r1 == 0. or r2 == 0. else r1 / r2
+        ratio = r1 / r2
+        # becomes NaN if ratio == NaN or 0, otherwise 0
+        nan_or_zero = 1 - ratio / ratio
+        r = where(nan_or_zero, ones_like(ratio), ratio)
         lr *= r
 
         # update weight
-        weight[:] -= lr * g
+        g *= lr
+        weight[:] -= g
