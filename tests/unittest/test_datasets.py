9c64d5b43fca9b8836ed424c2d2563831e7bf4bd
Leonard Lausen <lausen@amazon.com>
2019-10-14 23:29:21 +0000

    Disable Simverb test again

--- tests/unittest/test_datasets.py
+++ tests/unittest/test_datasets.py
@@ -268,8 +268,9 @@ def test_simlex999():
 @flaky(max_runs=2, min_passes=1)
 @pytest.mark.serial
 @pytest.mark.remote_required
-@pytest.mark.skipif(datetime.date.today() < datetime.date(2019, 10, 14),
-                    reason="simverb temporarily unavailable.")
+@pytest.mark.skipif(datetime.date.today() < datetime.date(2019, 10, 21),
+                    reason="simverb temporarily unavailable"
+                    "contacted author on 10/14")
 def test_simverb3500():
     data = nlp.data.SimVerb3500()
     assert len(data) == 3500
