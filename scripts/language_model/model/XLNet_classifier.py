fix bert classifier & fix attention mask

scripts/language_model/model/XLNet_classifier.py
@@ -55,7 +55,7 @@ class XLNetClassifier(Block):
         #we are using left pad
         F = mx.ndarray
         valid_length_start = valid_length_start.astype('int32')
-        steps = F.contrib.arange_like(inputs, axis=1)
+        steps = F.contrib.arange_like(inputs, axis=1) + 1
         ones = F.ones_like(steps)
         mask = F.broadcast_greater(F.reshape(steps, shape=(1, -1)),
                                    F.reshape(valid_length_start, shape=(-1, 1)))