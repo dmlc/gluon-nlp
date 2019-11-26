12117c9c5e761d0176261731503adfa34222d671
Wang <zechenw@a483e7462a21.ant.amazon.com>
2019-11-26 15:04:22 +0800

    make test faster

--- scripts/tests/test_scripts.py
+++ scripts/tests/test_scripts.py
@@ -346,9 +346,10 @@ def test_finetune_squad(sentencepiece):
 @pytest.mark.remote_required
 @pytest.mark.integration
 @pytest.mark.parametrize('dataset', ['MRPC'])
+@pytest.mark.parametrize('early_stop', )
 def test_xlnet_finetune_glue(dataset):
-    arguments = ['--batch_size', '12', '--task_name', dataset,
-                 '--gpu', '0', '--epochs', '1']
+    arguments = ['--batch_size', '32', '--task_name', dataset,
+                 '--gpu', '0', '--epochs', '1', '--max_len', '32']
     process = subprocess.check_call([sys.executable, './scripts/language_model/run_glue.py']
                                     + arguments)
     time.sleep(5)
