b82b0e1c376b0f1a8d3c696b56ca27383b7ca5b8
Wang <zechenw@a483e7462a21.ant.amazon.com>
2019-12-02 16:41:38 +0800

    new features&test

--- src/gluonnlp/data/__init__.py
+++ src/gluonnlp/data/__init__.py
@@ -38,10 +38,11 @@ from .translation import *
 from .utils import *
 from .word_embedding_evaluation import *
 from .intent_slot import *
+from .data_preprocessing_transform import *
 
 __all__ = (['batchify'] + utils.__all__ + transforms.__all__ + sampler.__all__
            + dataset.__all__ + corpora.__all__ + sentiment.__all__
            + word_embedding_evaluation.__all__ + stream.__all__ + conll.__all__
            + translation.__all__ + registry.__all__ + question_answering.__all__
            + dataloader.__all__ + candidate_sampler.__all__ + intent_slot.__all__
-           + glue.__all__)
+           + glue.__all__ + data_preprocessing_transform.__all__)
