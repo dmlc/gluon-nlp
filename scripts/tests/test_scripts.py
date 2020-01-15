447b631806331fa514896611434c5a838c8cf3fc
Leonard Lausen <leonard@lausen.nl>
2020-01-15 15:43:17 +0100

    Fix test invocation

--- scripts/tests/test_scripts.py
+++ scripts/tests/test_scripts.py
@@ -363,7 +363,7 @@ def test_bert_ner():
                  '--dev-path', 'sequence_labeling/dataset_sample/validation_sample.txt',
                  '--test-path', 'sequence_labeling/dataset_sample/test_sample.txt',
                  '--gpu', '0', '--learning-rate', '1e-5',
-                 '--epoch', '1', '--bert-model', 'bert_24_1024_16',
+                 '--num-epochs', '1', '--bert-model', 'bert_24_1024_16',
                  '--save-checkpoint-prefix', './test_bert_ner']
     script = './scripts/sequence_labeling/finetune_bert.py'
     process = subprocess.check_call([sys.executable, script] + arguments)
