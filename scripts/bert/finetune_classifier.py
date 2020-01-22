f7a2ea1e058195424cf49a9534091ab3f18cb2c5
Ubuntu <ubuntu@ip-172-31-13-184.ec2.internal>
2020-01-22 22:23:27 +0000

    amp patch

--- scripts/bert/finetune_classifier.py
+++ scripts/bert/finetune_classifier.py
@@ -181,6 +181,12 @@ parser.add_argument(
 
 args = parser.parse_args()
 
+# patch AMP due to issue: https://github.com/apache/incubator-mxnet/issues/17409
+ops = ['_contrib_interleaved_matmul_encdec_qk', '_contrib_interleaved_matmul_encdec_valatt',
+       '_contrib_interleaved_matmul_selfatt_qk', '_contrib_interleaved_matmul_selfatt_valatt']
+amp.lists.symbol.WIDEST_TYPE_CASTS.extend(ops)
+# end of the patch
+
 logging.getLogger().setLevel(logging.INFO)
 logging.captureWarnings(True)
 logging.info(args)
