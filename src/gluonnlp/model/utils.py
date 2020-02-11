3b62fdc528e412d62972911e93d5c5d735341fed
Lin <haibilin@a483e7be4c92.ant.amazon.com>
2020-02-10 18:04:20 -0800

    add err msg

--- src/gluonnlp/model/utils.py
+++ src/gluonnlp/model/utils.py
@@ -276,6 +276,7 @@ def _load_vocab(dataset_name, vocab, root, cls=None):
 
 def _load_pretrained_params(net, model_name, dataset_name, root, ctx, ignore_extra=False,
                             allow_missing=False):
+    assert model_name is not None, 'model_name is required when loading pretrained models.'
     path = '_'.join([model_name, dataset_name])
     model_file = model_store.get_model_file(path, root=root)
     net.load_parameters(model_file, ctx=ctx, ignore_extra=ignore_extra, allow_missing=allow_missing)
