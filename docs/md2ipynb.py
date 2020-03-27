1871419cca9e9e57de2470e77455e82b1c5145a4
Chen Wang <zhanghuilian-98022@163.com>
2020-03-27 22:06:27 +0800

    Adopt argparse for parsing arguments

--- docs/md2ipynb.py
+++ docs/md2ipynb.py
@@ -1,16 +1,15 @@
+import argparse
 import os
-import sys
 import time
-import notedown
-import nbformat
 
-help_message = 'Usage:' \
-               '  md2ipynb.py input.md [options]' \
-               '' \
-               'General Options:' \
-               '  -d                          Disable computing python scripts'
-assert len(sys.argv) > 1, help_message
-assert len(sys.argv) < 4, help_message
+import nbformat
+import notedown
+
+parser = argparse.ArgumentParser(description='Convert md file to ipynb files.')
+parser.add_argument('input', help='input.md', type=str)
+parser.add_argument('-d', '--disable_compute',
+                    help='Disable computing python scripts', action="store_true")
+args = parser.parse_args()
 
 # timeout for each notebook, in sec
 timeout = 40 * 60
@@ -18,14 +17,8 @@ timeout = 40 * 60
 # the files will be ignored for execution
 ignore_execution = []
 
-input_path = sys.argv[1]
-enable_compute = True
-if len(sys.argv) == 3:
-    if sys.argv[2] == '-d':
-        enable_compute = False
-
 # Change working directory to directory of input file
-input_dir, input_fn = os.path.split(input_path)
+input_dir, input_fn = os.path.split(args.input)
 os.chdir(input_dir)
 
 output_fn = '.'.join(input_fn.split('.')[:-1] + ['ipynb'])
@@ -38,9 +31,9 @@ with open(input_fn, encoding='utf-8', mode='r') as f:
 
 if not any([i in input_fn for i in ignore_execution]):
     tic = time.time()
-    if enable_compute:
+    if not args.d:
         notedown.run(notebook, timeout)
-    print('=== Finished evaluation in %f sec'%(time.time()-tic))
+    print('=== Finished evaluation in %f sec' % (time.time() - tic))
 
 # write
 # need to add language info to for syntax highlight
