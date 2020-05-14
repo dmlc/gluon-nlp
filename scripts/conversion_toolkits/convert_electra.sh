f2ea915fdd25e2042369365dc7d11daaceb75a2c
Xingjian Shi <xshiab@connect.ust.hk>
2020-05-13 17:05:37 -0700

    fix a commit id

--- scripts/conversion_toolkits/convert_electra.sh
+++ scripts/conversion_toolkits/convert_electra.sh
@@ -1,5 +1,8 @@
 export TF_FORCE_GPU_ALLOW_GROWTH="true"
 git clone https://github.com/ZheyuYe/electra.git
+cd electra
+git checkout 923179410471f9e1820b3f0771c239e1752e4e18
+cd ..
 for model in small base large
 do
     wget https://storage.googleapis.com/electra-data/electra_${model}.zip
