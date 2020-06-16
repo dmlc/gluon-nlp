97d0ad68b6cc92ad0ce98eff06118bfcf0e88a43
Hu <huta@a483e74650ff.ant.amazon.com>
2020-06-16 16:15:09 +0800

    added clean_tok_mono_corpus

--- scripts/machine_translation/wmt2014_back_translation.sh
+++ scripts/machine_translation/wmt2014_back_translation.sh
@@ -1,23 +1,25 @@
 SUBWORD_ALGO=$1
-SRC=de
+SRC=en
+TGT=de
 
 
 # Fetch the raw text
 nlp_data prepare_wmt \
         --mono \
-        --mono_lang ${SRC} \
+        --mono_lang ${TGT} \
         --dataset newscrawl \
         --save-path wmt2014_mono
 
 
 # Clean and tokenize the monolingual corpus
 cd wmt2014_mono
-nlp_preprocess clean_tok_para_corpus --mono \
-                        --lang ${SRC} \
-                        --src-corpus train.raw.${SRC} \
+nlp_preprocess clean_tok_mono_corpus \
+                        --lang ${TGT} \
+                        --corpus train.raw.${TGT} \
                         --min-num-words 1 \
                         --max-num-words 100 \
-                        --src-save-path train.tok.${SRC}
+                        --save-path train.tok.${TGT}
+                        --num-process 16
 
 
 # Apply the learned codes to the monolingual dataset
@@ -25,23 +27,23 @@ nlp_preprocess apply_subword --model ${SUBWORD_ALGO}\
                              --output-type subword \
                              --model-path ../wmt2014_ende/${SUBWORD_ALGO}.model \
                              --vocab-path ../wmt2014_ende/${SUBWORD_ALGO}.vocab \
-                             --corpus train.tok.${SRC} \
-                             --save-path train.tok.${SUBWORD_ALGO}.${SRC}
+                             --corpus train.tok.${TGT} \
+                             --save-path train.tok.${SUBWORD_ALGO}.${TGT}
 
 cd ../../../machine_translation
 
-# train the model to translate German to English
+# train the reverse model to translate German to English
 SUBWORD_MODEL=yttm
 python train_transformer.py \
-    --train_src_corpus ../datasets/machine_translation/wmt2014_ende/train.tok.${SUBWORD_MODEL}.de \
-    --train_tgt_corpus ../datasets/machine_translation/wmt2014_ende/train.tok.${SUBWORD_MODEL}.en \
-    --dev_src_corpus ../datasets/machine_translation/wmt2014_ende/dev.tok.${SUBWORD_MODEL}.de \
-    --dev_tgt_corpus ../datasets/machine_translation/wmt2014_ende/dev.tok.${SUBWORD_MODEL}.en \
+    --train_src_corpus ../datasets/machine_translation/wmt2014_ende/train.tok.${SUBWORD_MODEL}.${TGT} \
+    --train_tgt_corpus ../datasets/machine_translation/wmt2014_ende/train.tok.${SUBWORD_MODEL}.${SRC} \
+    --dev_src_corpus ../datasets/machine_translation/wmt2014_ende/dev.tok.${SUBWORD_MODEL}.${TGT} \
+    --dev_tgt_corpus ../datasets/machine_translation/wmt2014_ende/dev.tok.${SUBWORD_MODEL}.${SRC} \
     --src_subword_model_path ../datasets/machine_translation/wmt2014_ende/${SUBWORD_MODEL}.model \
     --src_vocab_path ../datasets/machine_translation/wmt2014_ende/${SUBWORD_MODEL}.vocab \
     --tgt_subword_model_path ../datasets/machine_translation/wmt2014_ende/${SUBWORD_MODEL}.model \
     --tgt_vocab_path ../datasets/machine_translation/wmt2014_ende/${SUBWORD_MODEL}.vocab \
-    --save_dir transformer_wmt2014_deen_yttm \
+    --save_dir transformer_wmt2014_de_en_yttm \
     --cfg wmt_de_en_base.yml \
     --lr 0.002 \
     --warmup_steps 4000 \
@@ -55,16 +57,16 @@ cp ./wmt2014_ende ./mono_dir
 
 # Due to the limited memory, we need to split the data and process the data divided respectively 
 cd mono_dir
-split -l 400000 train.raw.de raw_split -d -a 2
+split -l 400000 train.raw.${TGT} raw_split -d -a 2
 
 # Get the synthetic data
 cd ../../../machine_translation
 for NUM in $(seq -w 0 96); do \
     printf "%s %d\n" "NUM:" $NUM | \
     python evaluate_transformer.py \
-        --param_path transformer_wmt2014_deen_yttm/average.params \
-        --src_lang de \
-        --tgt_lang en \
+        --param_path transformer_wmt2014_de_en_yttm/average.params \
+        --src_lang ${TGT} \
+        --tgt_lang ${SRC} \
         --cfg wmt_en_de_base.yml \
         --src_tokenizer ${SUBWORD_MODEL} \
         --tgt_tokenizer ${SUBWORD_MODEL} \
@@ -82,10 +84,10 @@ done
 # Use the combine data to train the new model
 SUBWORD_MODEL=yttm
 python train_transformer.py \
-    --train_src_corpus ../datasets/machine_translation/backtranslation/train.tok.${SUBWORD_MODEL}.en \
-    --train_tgt_corpus ../datasets/machine_translation/backtranslation/train.tok.${SUBWORD_MODEL}.de \
-    --dev_src_corpus ../datasets/machine_translation/wmt2014_ende/dev.tok.${SUBWORD_MODEL}.en \
-    --dev_tgt_corpus ../datasets/machine_translation/wmt2014_ende/dev.tok.${SUBWORD_MODEL}.de \
+    --train_src_corpus ../datasets/machine_translation/backtranslation/train.tok.${SUBWORD_MODEL}.${SRC} \
+    --train_tgt_corpus ../datasets/machine_translation/backtranslation/train.tok.${SUBWORD_MODEL}.${TGT} \
+    --dev_src_corpus ../datasets/machine_translation/wmt2014_ende/dev.tok.${SUBWORD_MODEL}.${SRC} \
+    --dev_tgt_corpus ../datasets/machine_translation/wmt2014_ende/dev.tok.${SUBWORD_MODEL}.${TGT} \
     --src_subword_model_path ../datasets/machine_translation/wmt2014_ende/${SUBWORD_MODEL}.model \
     --src_vocab_path ../datasets/machine_translation/wmt2014_ende/${SUBWORD_MODEL}.vocab \
     --tgt_subword_model_path ../datasets/machine_translation/wmt2014_ende/${SUBWORD_MODEL}.model \
@@ -102,8 +104,8 @@ python train_transformer.py \
 # Finally, we can evaluate the model
 python evaluate_transformer.py \
     --param_path backtranslation_wmt2014_ende_yttm/average.params \
-    --src_lang en \
-    --tgt_lang de \
+    --src_lang ${SRC} \
+    --tgt_lang ${TGT} \
     --cfg wmt_en_de_base.yml \
     --src_tokenizer ${SUBWORD_MODEL} \
     --tgt_tokenizer ${SUBWORD_MODEL} \
@@ -111,7 +113,7 @@ python evaluate_transformer.py \
     --tgt_subword_model_path ../datasets/machine_translation/wmt2014_ende/${SUBWORD_MODEL}.model \
     --src_vocab_path ../datasets/machine_translation/wmt2014_ende/${SUBWORD_MODEL}.vocab \
     --tgt_vocab_path ../datasets/machine_translation/wmt2014_ende/${SUBWORD_MODEL}.vocab \
-    --src_corpus ../datasets/machine_translation/wmt2014_ende/test.raw.en \
-    --tgt_corpus ../datasets/machine_translation/wmt2014_ende/test.raw.de \
+    --src_corpus ../datasets/machine_translation/wmt2014_ende/test.raw.${SRC} \
+    --tgt_corpus ../datasets/machine_translation/wmt2014_ende/test.raw.${TGT} \
     --inference \
     --gpus 0,1,2,3
