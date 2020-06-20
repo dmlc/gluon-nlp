ccecd983b369267f998a5f20e0998742c490b24a
moisesh <moisesh@nvidia.com>
2020-06-19 17:11:52 -0700

    Add unitest for deploy bert script

--- scripts/tests/test_scripts.py
+++ scripts/tests/test_scripts.py
@@ -421,3 +421,24 @@ def test_xlnet_finetune_squad():
     process = subprocess.check_call([sys.executable, './scripts/language_model/run_squad.py']
                                     + arguments)
     time.sleep(5)
+
+@pytest.mark.serial
+@pytest.mark.gpu
+@pytest.mark.remote_required
+@pytest.mark.integration
+@pytest.mark.parametrize('bert_model', ['bert_12_768_12', 'bert_24_1024_16'])
+@pytest.mark.parametrize('task', ['SST', 'embedding', 'QA'])
+@pytest.mark.parametrize('dtype', ['float32', 'float16'])
+def test_deploy_bert(bert_model, task, dtype):
+    subprocess.check_call([sys.executable, "-m", "pip", "install", "-e", "scripts/bert"])
+    arguments = ['--bert_model', bert_model, '--task', task, '--dtype', dtype,
+                 '--gpu', '0', '--seq_length', '128', '--test_batch_size', '300',
+                 '--custom_pass', './scripts/bert/bertpass_lib.so', '--check_accuracy']
+    if dtype == 'float16':
+        os.environ['MXNET_FC_TRUE_FP16'] = '1'
+        os.environ['MXNET_SAFE_ACCUMULATION'] = '1'
+    process = subprocess.check_call([sys.executable, './scripts/bert/deploy.py']
+                                    + arguments)
+    os.environ['MXNET_FC_TRUE_FP16'] = '0'
+    os.environ['MXNET_SAFE_ACCUMULATION'] = '0'
+    time.sleep(5)
