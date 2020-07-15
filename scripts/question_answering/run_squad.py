3dd2dea3e070759f42edafe7596a3245da8cbf06
moisesh <moisesh@nvidia.com>
2020-07-15 14:58:15 -0700

    pass dtype as argument to get_backbone

--- scripts/question_answering/run_squad.py
+++ scripts/question_answering/run_squad.py
@@ -314,10 +314,7 @@ def get_network(model_name,
     # Create the network
     Model, cfg, tokenizer, download_params_path, _ = \
         get_backbone(model_name, load_backbone=not backbone_path)
-    cfg.defrost()
-    cfg.MODEL.dtype = dtype
-    cfg.freeze()
-    backbone = Model.from_cfg(cfg, use_pooler=False)
+    backbone = Model.from_cfg(cfg, use_pooler=False, dtype=dtype)
     # Load local backbone parameters if backbone_path provided.
     # Otherwise, download backbone parameters from gluon zoo.
 
