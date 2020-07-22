f407b8ed01e57a71d3a03f240fc478946a8fa345
Sheng Zha <szha@users.noreply.github.com>
2020-07-22 14:20:33 -0700

    [CI] Batch cpu version (#1275)
    * add python3-dev to batch docker
    
    * add a cpu queue

--- tools/batch/submit-job.py
+++ tools/batch/submit-job.py
@@ -17,7 +17,8 @@ parser.add_argument('--region', help='Default region when creating new connectio
 parser.add_argument('--name', help='name of the job', type=str, default='dummy')
 parser.add_argument('--job-type', help='type of job to submit.', type=str,
                     choices=['g4dn.4x', 'g4dn.8x', 'g4dn.12x', 'g4dn.16x',
-                             'p3.2x', 'p3.8x', 'p3.16x', 'p3dn.24x'], default='g4dn.4x')
+                             'p3.2x', 'p3.8x', 'p3.16x', 'p3dn.24x',
+                             'c5n.18x'], default='g4dn.4x')
 parser.add_argument('--source-ref',
                     help='ref in GluonNLP main github. e.g. numpy, refs/pull/500/head',
                     type=str, default='numpy')
@@ -92,7 +93,8 @@ job_definitions = {
     'p3.2x': 'gluon-nlp-1-jobs:5',
     'p3.8x': 'gluon-nlp-1-4gpu-jobs:2',
     'p3.16x': 'gluon-nlp-1-8gpu-jobs:1',
-    'p3dn.24x': 'gluon-nlp-1-8gpu-jobs:2'
+    'p3dn.24x': 'gluon-nlp-1-8gpu-jobs:2',
+    'c5n.18x': 'gluon-nlp-1-cpu-jobs:2',
 }
 
 def main():
