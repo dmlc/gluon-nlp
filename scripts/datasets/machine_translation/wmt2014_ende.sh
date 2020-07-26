ab6318916cd67c6b3e7d8664ac1a4316e1e07448
Hu <huta@a483e74650ff.ant.amazon.com>
2020-07-26 13:47:02 +0800

    fix

--- scripts/datasets/machine_translation/wmt2014_ende.sh
+++ scripts/datasets/machine_translation/wmt2014_ende.sh
@@ -34,7 +34,6 @@ nlp_preprocess clean_tok_para_corpus --src-lang ${SRC} \
                       --tgt-corpus dev.raw.${TGT} \
                       --min-num-words 1 \
                       --max-num-words 100 \
-                      --max-ratio 1.5 \
                       --src-save-path dev.tok.${SRC} \
                       --tgt-save-path dev.tok.${TGT}
 
