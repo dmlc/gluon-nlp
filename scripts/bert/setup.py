5f8a41e79231716baea35fe9acda9f704c50210d
root <moisesh@nvidia.com>
2020-08-01 20:48:25 +0000

    fixing trigger custom pass compilation

--- scripts/bert/setup.py
+++ scripts/bert/setup.py
@@ -7,11 +7,31 @@ import sys
 import os
 import logging
 from setuptools import setup
+from distutils.command.install import install
 import mxnet
 
 requirements = [
     'numpy>=1.16.0',
 ]
+
+def CompileBERTCustomPass():
+    logging.basicConfig(stream=sys.stderr, level=logging.INFO)
+    log = logging.getLogger()
+    out_lib_file = 'bertpass_lib.so'
+    log.info(' ... compiling BERT custom graph pass library into %s', out_lib_file)
+    mxnet_path = pathlib.Path(mxnet.__file__).parent.absolute()
+    mxnet_include_path = pathlib.Path.joinpath(mxnet_path, 'include/mxnet')
+    pass_path = os.path.dirname(os.path.realpath(__file__))
+    target = pass_path + '/' + out_lib_file
+    source = pass_path + '/bertpass_gpu.cc'
+    os.system('g++ -shared -fPIC -std=c++11 ' + source +
+              ' -o ' + target + ' -I ' +
+              str(mxnet_include_path))
+
+class CompileBERTPass(install):
+    def run(self):
+        install.run(self)
+        self.execute(CompileBERTCustomPass, ())
 
 setup(
     # Metadata
@@ -21,15 +41,6 @@ setup(
     author_email='mxnet-gluon@amazon.com',
     url='https://github.com/dmlc/gluon-nlp',
     description='MXNet Gluon NLP Toolkit - BERT scripts',
-    license='Apache-2.0'
+    license='Apache-2.0',
+    cmdclass={'install': CompileBERTPass}
 )
-
-# compile custom graph pass for bert
-logging.basicConfig(stream=sys.stderr, level=logging.INFO)
-log = logging.getLogger()
-out_lib_file = 'bertpass_lib.so'
-log.info(' ... compiling BERT custom graph pass library into %s', out_lib_file)
-mxnet_path = pathlib.Path(mxnet.__file__).parent.absolute()
-mxnet_include_path = pathlib.Path.joinpath(mxnet_path, 'include/mxnet')
-os.system('g++ -shared -fPIC -std=c++11 bertpass_gpu.cc -o bertpass_lib.so -I ' +
-          str(mxnet_include_path))
