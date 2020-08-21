ad7c2eef7a5d1c4785715553dc801c1db317c46e
barry-jin <barryjin1995@gmail.com>
2020-08-21 13:59:14 -0700

    [CI] Add GPU pytest + Submit jobs to AWS Batch through GitHub Actions

---
+++ test.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Shell script for installing dependencies and running test on AWS Batch
+
+# alias python3='/usr/bin/python3'
+
+python3 -m pip install --user -upgrade pip
+python3 -m pip install --user setuptools pytest pytest-cov contextvars
+python3 -m pip install --upgrade cython
+python3 -m pip install --pre --user "mxnet-cu102>=2.0.0b20200802" -f https://dist.mxnet.io/python
+python3 -m pip install --user -e .[extras]
+python3 -m pytest --cov=./ --cov-report=xml --durations=50 tests/
