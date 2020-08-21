925eb73d67656d62c44fd287f4ac6a6ec860eb11
Yuma1L <Yuma1L@Zheyu-mac.local>
2020-08-22 02:12:11 +0800

    fix typo

--- tools/batch/run_batch_conversion.sh
+++ tools/batch/run_batch_conversion.sh
@@ -6,5 +6,5 @@ python3 tools/batch/submit-job.py \
     --job-type g4dn.4x \
     --name convert_${MODEL_NAME} \
     --work-dir scripts/conversion_toolkits \
-    --remote https://github.com/dmlcgluon-nlp/ \
+    --remote https://github.com/dmlc/gluon-nlp/ \
     --command 'bash convert_'${MODEL_NAME}'.sh | tee stdout.log'
