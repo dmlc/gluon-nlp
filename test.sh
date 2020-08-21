84fac9110d845e0ee0e1907a81344646dd284264
barry-jin <barryjin1995@gmail.com>
2020-08-21 15:44:52 -0700

    [CI] Update CI pipeline

--- test.sh
+++ test.sh
@@ -3,9 +3,10 @@
 
 # alias python3='/usr/bin/python3'
 
+sudo apt-get install libopenblas-dev
 python3 -m pip install --user -upgrade pip
 python3 -m pip install --user setuptools pytest pytest-cov contextvars
 python3 -m pip install --upgrade cython
 python3 -m pip install --pre --user "mxnet-cu102>=2.0.0b20200802" -f https://dist.mxnet.io/python
 python3 -m pip install --user -e .[extras]
-python3 -m pytest --cov=./ --cov-report=xml --durations=50 tests/
+python3 -m pytest --cov=./ --cov-report=xml --durations=50 --device="mx.gpu()" tests/
