39d23512a279d07f5bae0f4d387b4740b7257b9f
barry-jin <barryjin1995@gmail.com>
2020-08-24 12:57:28 -0700

    [CI] Update path of /test

--- test.sh
+++ test.sh
@@ -3,10 +3,12 @@
 
 # alias python3='/usr/bin/python3'
 
+echo $PWD
+
 sudo apt-get install libopenblas-dev
 python3 -m pip install --user -upgrade pip
 python3 -m pip install --user setuptools pytest pytest-cov contextvars
 python3 -m pip install --upgrade cython
 python3 -m pip install --pre --user "mxnet-cu102>=2.0.0b20200802" -f https://dist.mxnet.io/python
 python3 -m pip install --user -e .[extras]
-python3 -m pytest --cov=./ --cov-report=xml --durations=50 --device="gpu" tests/
+python3 -m pytest --cov=./ --cov-report=xml --durations=50 --device="gpu" ../../tests/
