202a1ddc56407ef839d40bf905bbbe60452d0c5e
Xingjian Shi <xshiab@connect.ust.hk>
2020-08-27 12:42:07 -0700

    Update test_data_pipeline.sh

--- tests/batch_scripts/test_data_pipeline.sh
+++ tests/batch_scripts/test_data_pipeline.sh
@@ -1,6 +1,6 @@
 set -ex
 
-python3 -m pip install --pre "mxnet-cu102>=2.0.0b20200802" -f https://dist.mxnet.io/python
+python3 -m pip install --pre "mxnet>=2.0.0b20200802" -f https://dist.mxnet.io/python
 cd ../../scripts/datasets/machine_translation
 for MODEL in spm subword_nmt yttm hf_bytebpe hf_wordpiece hf_bpe
 do
