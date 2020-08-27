6995a35e7f8d6a51ba1adb4e556b9548eff83bde
Xingjian Shi <xshiab@connect.ust.hk>
2020-08-27 03:21:29 -0700

    Update test_data_pipeline.sh

--- tests/batch_scripts/test_data_pipeline.sh
+++ tests/batch_scripts/test_data_pipeline.sh
@@ -1,10 +1,12 @@
-python3 -m pip install --user -upgrade pip
-python3 -m pip install --user setuptools pytest pytest-cov contextvars
-python3 -m pip install --upgrade --force-reinstall langid --user
+set -ex
+
+python3 -m pip install --upgrade pip
+python3 -m pip installsetuptools pytest pytest-cov contextvars
+python3 -m pip install --upgrade --force-reinstall langid==1.15
 python3 -m pip install --upgrade cython
 
-python3 -m pip install --pre --user "mxnet-cu102>=2.0.0b20200802" -f https://dist.mxnet.io/python
-python3 -m pip install --user -e .[extras]
+python3 -m pip install --pre "mxnet-cu102>=2.0.0b20200802" -f https://dist.mxnet.io/python
+python3 -m pip install -e .[extras]
 cd ../../scripts/datasets/machine_translation
 for MODEL in spm subword_nmt yttm hf_bytebpe hf_wordpiece hf_bpe
   bash wmt2014_ende.sh ${MODEL}
