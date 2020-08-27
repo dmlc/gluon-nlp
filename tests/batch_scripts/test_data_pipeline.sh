18a62a7696e88354a2f337f7d9736bdd5472ac94
Xingjian Shi <xshiab@connect.ust.hk>
2020-08-27 02:25:03 -0700

    Update test_data_pipeline.sh

--- tests/batch_scripts/test_data_pipeline.sh
+++ tests/batch_scripts/test_data_pipeline.sh
@@ -1,3 +1,7 @@
+python3 -m pip install --user -upgrade pip
+python3 -m pip install --user setuptools pytest pytest-cov contextvars
+python3 -m pip install --upgrade cython
+
 python3 -m pip install --pre --user "mxnet-cu102>=2.0.0b20200802" -f https://dist.mxnet.io/python
 python3 -m pip install --user -e .[extras]
 cd scripts/datasets/machine_translation
