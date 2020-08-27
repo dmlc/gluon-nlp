521c2370f467db904ab937b68a5b98ec00c4e321
Xingjian Shi <xshiab@connect.ust.hk>
2020-08-27 02:26:35 -0700

    Update test_data_pipeline.sh

--- tests/batch_scripts/test_data_pipeline.sh
+++ tests/batch_scripts/test_data_pipeline.sh
@@ -4,7 +4,7 @@ python3 -m pip install --upgrade cython
 
 python3 -m pip install --pre --user "mxnet-cu102>=2.0.0b20200802" -f https://dist.mxnet.io/python
 python3 -m pip install --user -e .[extras]
-cd scripts/datasets/machine_translation
+cd ../../scripts/datasets/machine_translation
 for MODEL in spm subword_nmt yttm hf_bytebpe hf_wordpiece hf_bpe
   bash wmt2014_ende.sh ${MODEL}
 for MODEL in spm subword_nmt yttm hf_bytebpe hf_wordpiece hf_bpe
