b81e5f61f519ddda764e98235e3f3408d9dbe612
Xingjian Shi <xshiab@connect.ust.hk>
2020-08-31 01:09:50 -0700

    Update test_learn_apply_subword.py

--- tests/process_cli/test_learn_apply_subword.py
+++ tests/process_cli/test_learn_apply_subword.py
@@ -18,13 +18,14 @@ def get_test_pair_corpus(key='de-en'):
 
 
 def verify_subword_algorithms_ende(dir_path):
+    dir_path = os.path.realpath(dir_path)
     parser = learn_subword.get_parser()
     apply_parser = apply_subword.get_parser()
     corpus_path_pair = [os.path.join(_CURR_DIR, 'data', 'wmt19-test-de-en.de'),
                         os.path.join(_CURR_DIR, 'data', 'wmt19-test-de-en.en')]
     for model in ['yttm', 'spm', 'subword_nmt', 'hf_bpe', 'hf_bytebpe', 'hf_wordpiece']:
         args = parser.parse_args(['--corpus'] + corpus_path_pair +
-                                 ['--model', model, '--vocab-size', 5000,
+                                 ['--model', model, '--vocab-size', '5000',
                                   '--save-dir', dir_path])
         # Train the tokenizer
         learn_subword.main(args)
