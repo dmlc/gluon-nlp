76499bafe41c20094b5f592c62659d5c2afa9037
Xingjian Shi <xshiab@connect.ust.hk>
2020-09-02 15:47:24 -0700

    Update test.sh

--- tools/batch/batch_states/test.sh
+++ tools/batch/batch_states/test.sh
@@ -3,10 +3,7 @@
 
 echo $PWD
 
-sudo apt-get install libopenblas-dev
 python3 -m pip install --user --quiet -upgrade pip
 python3 -m pip install --user --quiet setuptools pytest pytest-cov contextvars
 python3 -m pip install --upgrade --quiet cython
-python3 -m pip install --pre --user --quiet "mxnet-cu102>=2.0.0b20200802" -f https://dist.mxnet.io/python
-python3 -m pip install --user --quiet -e .[extras]
-python3 -m pytest --cov=./ --cov-report=xml --durations=50 --device="gpu" --runslow /gluon-nlp/tests/
+python3 -m pytest --cov=./ --cov-report=xml --durations=50 --device="gpu" --runslow ../../../tests/
