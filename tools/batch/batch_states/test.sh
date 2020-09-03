8de9490c526bfdf4a0daa3b1c68baefd4c6d6ef5
Xingjian Shi <xshiab@connect.ust.hk>
2020-09-03 01:00:56 -0700

    Update test.sh

--- tools/batch/batch_states/test.sh
+++ tools/batch/batch_states/test.sh
@@ -6,4 +6,4 @@ echo $PWD
 python3 -m pip install --user --quiet -upgrade pip
 python3 -m pip install --user --quiet setuptools pytest pytest-cov contextvars
 python3 -m pip install --upgrade --quiet cython
-python3 -m pytest --cov=./ --cov-report=xml --durations=50 --device="gpu" --runslow /gluon-nlp/tests/
+python3 -m pytest --cov=/gluon-nlp --cov-config=/gluon-nlp/.coveragerc --cov-report=xml --durations=50 --device="gpu" --runslow /gluon-nlp/tests/
