6838434f7b0ffb668cb23541e7cca1c2dd605e37
barry-jin <barryjin1995@gmail.com>
2020-09-03 18:33:28 -0700

    [CI] Remove assert

--- tests/test_models.py
+++ tests/test_models.py
@@ -23,7 +23,6 @@ def test_get_backbone(name, ctx):
         assert num_params > 0
 
         # Test for model export + save
-        assert False
         batch_size = 1
         sequence_length = 4
         inputs = mx.np.random.randint(0, 10, (batch_size, sequence_length))
