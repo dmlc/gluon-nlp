7f29267155f9afa0429a323e22fdc918ddb52358
MoisesHer <50716238+MoisesHer@users.noreply.github.com>
2020-09-10 09:06:31 -0700

    Deploy BERT model -  Script (#1237)
    * Add example script to deploy BERT
    
    * Add options to better measure performance
    
    * Allow specification of path for exported model
    
    * Add option to use custom graph pass
    
    * Add optimization for MHA in custom graph pass
    
    * Correct bug with input shapes in optimize_for
    
    * correct typo
    
    * fix lint
    
    * fix lint
    
    * Add documentation
    
    * Add documentation for using deploy script
    
    * Correct typo/add spaces in documentation
    
    * Add setup.py to compile pass, update documentation
    
    * Fix bug in path to include dir & fix pylint
    
    * Add unitest for deploy bert script
    
    * change CUDA version in wheel
    
    * test latest wheel
    
    * change path to custom pass library
    
    * fixing trigger custom pass compilation
    
    * fix lint
    
    * fix lint
    
    * Update mxnet pip version
    
    * Only GPU versions changed
    
    * fix lint
    
    * change wheel to include mkl headers
    
    * lint docstring
    
    * remove debug print
    
    * change include paths
    
    * lint
    
    * debugging lib_api.h
    
    * debugging lib_api.h
    
    * debugging
    
    * Disable test for now
    
    * skip test if mxnet_version < 1.7.0
    
    * use pytest.mark.skipif to skip test
    
    * test only BERT-base (fp16/fp32, SST/QA, embeddings) to avoid timeout
    
    Co-authored-by: Leonard Lausen <lausen@amazon.com>

--- scripts/tests/test_scripts.py
+++ scripts/tests/test_scripts.py
@@ -421,3 +421,25 @@ def test_xlnet_finetune_squad():
     process = subprocess.check_call([sys.executable, './scripts/language_model/run_squad.py']
                                     + arguments)
     time.sleep(5)
+
+@pytest.mark.skipif(mx.__version__ < '1.7.0', reason="Requires MXNet 1.7 or higher")
+@pytest.mark.serial
+@pytest.mark.gpu
+@pytest.mark.remote_required
+@pytest.mark.integration
+@pytest.mark.parametrize('bert_model', ['bert_12_768_12'])
+@pytest.mark.parametrize('task', ['SST', 'embedding', 'QA'])
+@pytest.mark.parametrize('dtype', ['float32', 'float16'])
+def test_deploy_bert(bert_model, task, dtype):
+    subprocess.check_call([sys.executable, './scripts/bert/setup.py', 'install'])
+    arguments = ['--bert_model', bert_model, '--task', task, '--dtype', dtype,
+                 '--gpu', '0', '--seq_length', '128', '--test_batch_size', '300',
+                 '--custom_pass', 'scripts/bert/bertpass_lib.so', '--check_accuracy']
+    if dtype == 'float16':
+        os.environ['MXNET_FC_TRUE_FP16'] = '1'
+        os.environ['MXNET_SAFE_ACCUMULATION'] = '1'
+    process = subprocess.check_call([sys.executable, './scripts/bert/deploy.py']
+                                    + arguments)
+    os.environ['MXNET_FC_TRUE_FP16'] = '0'
+    os.environ['MXNET_SAFE_ACCUMULATION'] = '0'
+    time.sleep(5)
