bcaa5aafeafc519cd26f3c95793a4f3fced5e383
Xingjian Shi <xshiab@connect.ust.hk>
2020-09-13 20:02:14 -0700

    Update learn_subword.py

--- scripts/processing/learn_subword.py
+++ scripts/processing/learn_subword.py
@@ -108,6 +108,7 @@ def main(args):
     model_prefix = os.path.join(args.save_dir, args.model)
     print('Save the subword model to {}.model'.format(model_prefix))
     print('Save the vocabulary to {}.vocab'.format(model_prefix))
+    print('------- Start Training -------------')
     special_tokens_kv = OrderedDict()
     if not args.disable_unk:
         special_tokens_kv['unk_token'] = Vocab.UNK_TOKEN
@@ -311,7 +312,7 @@ def main(args):
         raise NotImplementedError
     vocab_obj = Vocab(vocab, **special_tokens_kv)
     vocab_obj.save(model_prefix + '.vocab')
-
+    print('------- Done -------------')
 
 def cat_corpus(corpus_path_list):
     # TODO Use temporary file
