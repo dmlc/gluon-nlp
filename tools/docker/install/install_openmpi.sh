fc665513b780816a195fdf9276935bdf7ca61fdb
Xingjian Shi <xshiab@connect.ust.hk>
2020-10-09 18:42:55 -0700

    Update install_openmpi.sh

--- tools/docker/install/install_openmpi.sh
+++ tools/docker/install/install_openmpi.sh
@@ -1,6 +1,6 @@
 set -euo pipefail
 
-RUN mkdir /tmp/openmpi \
+mkdir /tmp/openmpi \
  && cd /tmp/openmpi \
  && curl -fSsL -O https://download.open-mpi.org/release/open-mpi/v4.0/openmpi-4.0.1.tar.gz \
  && tar zxf openmpi-4.0.1.tar.gz \
@@ -12,10 +12,10 @@ RUN mkdir /tmp/openmpi \
  && rm -rf /tmp/openmpi
 
 # Create a wrapper for OpenMPI to allow running as root by default
-RUN mv /usr/local/bin/mpirun /usr/local/bin/mpirun.real \
+mv /usr/local/bin/mpirun /usr/local/bin/mpirun.real \
  && echo '#!/bin/bash' > /usr/local/bin/mpirun \
  && echo 'mpirun.real --allow-run-as-root "$@"' >> /usr/local/bin/mpirun \
  && chmod a+x /usr/local/bin/mpirun
 
-RUN echo "hwloc_base_binding_policy = none" >> /usr/local/etc/openmpi-mca-params.conf \
+echo "hwloc_base_binding_policy = none" >> /usr/local/etc/openmpi-mca-params.conf \
  && echo "rmaps_base_mapping_policy = slot" >> /usr/local/etc/openmpi-mca-params.conf
