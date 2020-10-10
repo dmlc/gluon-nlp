5790d6b628c0e3208f3ca127903bff5bf817c92c
Xingjian Shi <xshiab@connect.ust.hk>
2020-10-09 20:54:35 -0700

    Update run_squad2_albert_base.sh

--- scripts/question_answering/commands/run_squad2_albert_base.sh
+++ scripts/question_answering/commands/run_squad2_albert_base.sh
@@ -1,3 +1,5 @@
+USE_HOROVOD=${1:-0}  # Horovod flag. Do not use horovod by default
+
 VERSION=2.0  # Either 2.0 or 1.1
 MODEL_NAME=google_albert_base_v2
 
@@ -6,8 +8,14 @@ nlp_data prepare_squad --version ${VERSION}
 
 # Run the script
 
-python3 run_squad.py \
-    --model_name ${MODEL_NAME} \
+if [ ${USE_HOROVOD} -eq 0 ];
+then
+  RUN_COMMAND="python3 run_squad.py --gpus 0,1,2,3"
+else
+  RUN_COMMAND="horovodrun -np 4 -H localhost:4 python3 run_squad.py --comm_backend horovod"
+fi
+
+${RUN_COMMAND} --model_name ${MODEL_NAME} \
     --data_dir squad \
     --output_dir fintune_${MODEL_NAME}_squad_${VERSION} \
     --version ${VERSION} \
@@ -15,11 +23,10 @@ python3 run_squad.py \
     --do_train \
     --batch_size 4 \
     --num_accumulated 3 \
-    --gpus 0,1,2,3 \
     --epochs 3 \
     --lr 2e-5 \
     --warmup_ratio 0.1 \
     --wd 0.01 \
     --max_seq_length 512 \
     --max_grad_norm 0.1 \
-    --overwrite_cache \
+    --overwrite_cache
