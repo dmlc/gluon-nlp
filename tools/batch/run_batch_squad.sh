0b8f37ddc697727700b6fa9475bca9f835037099
Xingjian Shi <xshiab@connect.ust.hk>
2020-10-10 23:58:51 -0700

    Update run_batch_squad.sh

--- tools/batch/run_batch_squad.sh
+++ tools/batch/run_batch_squad.sh
@@ -1,3 +1,7 @@
+USE_HOROVOD=${1:-0}
+VERSION=${2:-2.0}
+LOG_PATH=${3:-submit_squad_v2.log}
+
 for MODEL_NAME in albert_base \
                   albert_large \
                   albert_xlarge \
@@ -18,5 +22,5 @@ do
       --name test_squad2_${MODEL_NAME} \
       --work-dir scripts/question_answering \
       --remote https://github.com/dmlc/gluon-nlp/ \
-      --command 'bash commands/run_squad2_'${MODEL_NAME}'.sh | tee stdout.log' >> submit_squad_v2.log
+      --command 'bash commands/run_squad2_'${MODEL_NAME}'.sh ${USE_HOROVOD} ${VERSION} | tee stdout.log' >> ${LOG_PATH}
 done
