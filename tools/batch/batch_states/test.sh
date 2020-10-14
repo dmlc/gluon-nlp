ce551c8628bf51a004434e0721a49234ab4829d5
Xingjian Shi <xshiab@connect.ust.hk>
2020-10-14 12:54:47 -0700

    Update test.sh

--- tools/batch/batch_states/test.sh
+++ tools/batch/batch_states/test.sh
@@ -4,6 +4,6 @@ set -ex
 
 echo $PWD
 SCRIPTPATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
-REPODIR=${SCRIPTPATH}/../../../
+REPODIR=${SCRIPTPATH}/../../../../gluon-nlp
 
 python3 -m pytest --cov=$REPODIR --cov-config=$REPODIR/.coveragerc --cov-report=xml --durations=50 --device="gpu" --runslow $REPODIR/tests/
