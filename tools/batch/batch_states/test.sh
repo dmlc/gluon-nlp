b8046a0ddd00ecdf0f3d869c6030d5fea144db21
Xingjian Shi <xshiab@connect.ust.hk>
2020-10-14 12:52:21 -0700

    Update test.sh

--- tools/batch/batch_states/test.sh
+++ tools/batch/batch_states/test.sh
@@ -3,7 +3,7 @@
 set -ex
 
 echo $PWD
-DIRNAME=$(dirname $0)
-REPODIR=$DIRNAME/../../../
+SCRIPTPATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+REPODIR=${SCRIPTPATH}/../../../
 
 python3 -m pytest --cov=$REPODIR --cov-config=$REPODIR/.coveragerc --cov-report=xml --durations=50 --device="gpu" --runslow $REPODIR/tests/
