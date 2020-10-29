dcdb91e5d386cdaf5bb43abe46443e9997a07eaf
Xingjian Shi <xshiab@connect.ust.hk>
2020-10-29 15:43:06 -0700

    Update test_wikipedia.py

--- tests/data_cli/test_wikipedia.py
+++ tests/data_cli/test_wikipedia.py
@@ -10,6 +10,7 @@ from gluonnlp.cli.data.pretrain_corpus import prepare_wikipedia
 def test_download_format(lang):
     parser = prepare_wikipedia.get_parser()
     with tempfile.TemporaryDirectory() as root:
-        download_args = parser.parse_args(['--mode', 'download', '--lang', lang,
+        download_args = parser.parse_args(['--mode', 'download+format',
+                                           '--lang', lang,
                                            '--date', 'latest', '-o', root])
         prepare_wikipedia.main(download_args)
