ae1f3fd27c1c0e993431252f9a1f138032de7ed0
Xingjian Shi <xshiab@connect.ust.hk>
2020-10-29 16:53:18 -0700

    Update test_wikipedia.py

--- tests/data_cli/test_wikipedia.py
+++ tests/data_cli/test_wikipedia.py
@@ -1,12 +1,11 @@
-import os
 import pytest
 import tempfile
 from gluonnlp.cli.data.pretrain_corpus import prepare_wikipedia
 
 
 @pytest.mark.remote_required
-# Test for zh-classical + hindi, which are smaller compared with English
-@pytest.mark.parametrize('lang', ['zh-classical', 'hi'])
+# Test for zh-classical (文言) + wuu (吴语), which are smaller compared with English
+@pytest.mark.parametrize('lang', ['zh-classical', 'wuu'])
 def test_download_format(lang):
     parser = prepare_wikipedia.get_parser()
     with tempfile.TemporaryDirectory() as root:
