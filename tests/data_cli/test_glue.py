5145e2ca926e87a0415cdace0cf1893938f03400
Xingjian Shi <xshiab@connect.ust.hk>
2020-10-29 19:37:49 -0700

    Update test_glue.py

--- tests/data_cli/test_glue.py
+++ tests/data_cli/test_glue.py
@@ -5,10 +5,24 @@ from gluonnlp.cli.data.general_nlp_benchmark import prepare_glue
 
 
 @pytest.mark.remote_required
-@pytest.mark.parametrize('benchmark', ['glue', 'superglue'])
-def test_glue_superglue(benchmark):
+@pytest.mark.parametrize('task', ["cola", "sst", "mrpc", "qqp", "sts", "mnli",
+                                  "snli", "qnli", "rte", "wnli", "diagnostic"])
+def test_glue(task):
     parser = prepare_glue.get_parser()
     with tempfile.TemporaryDirectory() as root:
-        args = parser.parse_args(['--benchmark', benchmark,
+        args = parser.parse_args(['--benchmark', 'glue',
+                                  '--tasks', task,
+                                  '--data_dir', root])
+        prepare_glue.main(args)
+
+
+@pytest.mark.remote_required
+@pytest.mark.parametrize('task', ["cb", "copa", "multirc", "rte", "wic", "wsc", "boolq", "record",
+                                  'broadcoverage-diagnostic', 'winogender-diagnostic'])
+def test_glue(task):
+    parser = prepare_glue.get_parser()
+    with tempfile.TemporaryDirectory() as root:
+        args = parser.parse_args(['--benchmark', 'glue',
+                                  '--tasks', task,
                                   '--data_dir', root])
         prepare_glue.main(args)
