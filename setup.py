e0a5113b1407231fac07073f63c2b37bdbbf6056
barry-jin <barryjin1995@gmail.com>
2020-12-07 15:43:01 -0800

    fix numpy version

--- setup.py
+++ setup.py
@@ -76,7 +76,7 @@ setup(
     install_requires=requirements,
     extras_require={
         'extras': [
-            'numpy==1.19.4'
+            'numpy==1.19.4',
             'boto3',
             'tqdm',
             'jieba',
