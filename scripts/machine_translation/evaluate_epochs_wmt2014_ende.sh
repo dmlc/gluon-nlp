d00040334dc4338ef101d9f6394c33138ef7906f
Xingjian Shi <xshiab@connect.ust.hk>
2020-12-30 15:15:56 -0800

    Update evaluate_epochs_wmt2014_ende.sh

--- scripts/machine_translation/evaluate_epochs_wmt2014_ende.sh
+++ scripts/machine_translation/evaluate_epochs_wmt2014_ende.sh
@@ -10,41 +10,25 @@ BEAM_SIZE=${8:-4}
 
 for epoch in $( seq ${EPOCH_BEGIN} ${EPOCH_END})
 do
-    python3 evaluate_transformer.py \
-        --param_path ${SAVE_DIR}/epoch${epoch}.params \
-        --src_lang en \
-        --tgt_lang de \
-        --cfg ${SAVE_DIR}/config.yml \
-        --src_tokenizer ${SUBWORD_ALGO} \
-        --tgt_tokenizer ${SUBWORD_ALGO} \
-        --src_subword_model_path wmt2014_ende/${SUBWORD_ALGO}.model \
-        --tgt_subword_model_path wmt2014_ende/${SUBWORD_ALGO}.model \
-        --src_vocab_path wmt2014_ende/${SUBWORD_ALGO}.vocab \
-        --tgt_vocab_path wmt2014_ende/${SUBWORD_ALGO}.vocab \
-        --src_corpus wmt2014_ende/test.raw.en \
-        --tgt_corpus wmt2014_ende/test.raw.de \
-        --lp_alpha ${LP_ALPHA} \
-        --lp_k ${LP_K} \
-        --beam-size ${BEAM_SIZE} \
-        --save_dir ${SAVE_DIR}/epoch${epoch}_evaluation_alpha${LP_ALPHA}_K${LP_K}_beam${BEAM_SIZE} \
-        --fp16
-
-    python3 evaluate_transformer.py \
-        --param_path ${SAVE_DIR}/epoch${epoch}.params \
-        --src_lang en \
-        --tgt_lang de \
-        --cfg ${SAVE_DIR}/config.yml \
-        --src_tokenizer ${SUBWORD_ALGO} \
-        --tgt_tokenizer ${SUBWORD_ALGO} \
-        --src_subword_model_path wmt2014_ende/${SUBWORD_ALGO}.model \
-        --tgt_subword_model_path wmt2014_ende/${SUBWORD_ALGO}.model \
-        --src_vocab_path wmt2014_ende/${SUBWORD_ALGO}.vocab \
-        --tgt_vocab_path wmt2014_ende/${SUBWORD_ALGO}.vocab \
-        --src_corpus wmt2014_ende/dev.raw.en \
-        --tgt_corpus wmt2014_ende/dev.raw.de \
-        --lp_alpha ${LP_ALPHA} \
-        --lp_k ${LP_K} \
-        --beam-size ${BEAM_SIZE} \
-        --save_dir ${SAVE_DIR}/epoch${epoch}_evaluation_dev_alpha${LP_ALPHA}_K${LP_K}_beam${BEAM_SIZE} \
-        --fp16
+    for fold in dev test
+    do
+        python3 evaluate_transformer.py \
+          --param_path ${SAVE_DIR}/epoch${epoch}.params \
+          --src_lang en \
+          --tgt_lang de \
+          --cfg ${SAVE_DIR}/config.yml \
+          --src_tokenizer ${SUBWORD_ALGO} \
+          --tgt_tokenizer ${SUBWORD_ALGO} \
+          --src_subword_model_path wmt2014_ende/${SUBWORD_ALGO}.model \
+          --tgt_subword_model_path wmt2014_ende/${SUBWORD_ALGO}.model \
+          --src_vocab_path wmt2014_ende/${SUBWORD_ALGO}.vocab \
+          --tgt_vocab_path wmt2014_ende/${SUBWORD_ALGO}.vocab \
+          --src_corpus wmt2014_ende/${fold}.raw.en \
+          --tgt_corpus wmt2014_ende/${fold}.raw.de \
+          --lp_alpha ${LP_ALPHA} \
+          --lp_k ${LP_K} \
+          --beam-size ${BEAM_SIZE} \
+          --save_dir ${SAVE_DIR}/epoch${epoch}_evaluation_${fold}_alpha${LP_ALPHA}_K${LP_K}_beam${BEAM_SIZE} \
+          --fp16
+    done
 done
