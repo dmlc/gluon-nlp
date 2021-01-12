aa50b3113d6d5ca8a20c886d05180acf0ecdb2ad
Ubuntu <ubuntu@ip-10-21-5-165.us-west-2.compute.internal>
2021-01-12 02:52:28 +0000

    remove useless code

--- scripts/datasets/pretrain_corpus/prepare_wikipedia.py
+++ scripts/datasets/pretrain_corpus/prepare_wikipedia.py
@@ -109,27 +109,6 @@ def merge(x):
                             article_lines.append(line)
 
 
-
-class BookscorpusTextFormatting:
-    def __init__(self, books_path, output_filename, recursive = False):
-        self.books_path = books_path
-        self.recursive = recursive
-        self.output_filename = output_filename
-
-
-    # This puts one book per line
-    def merge(self):
-        with open(self.output_filename, mode='w', newline='\n') as ofile:
-            for filename in glob.glob(self.books_path + '/' + '*.txt', recursive=True):
-                with open(filename, mode='r', encoding='utf-8-sig', newline='\n') as file:
-                    for line in file:
-                        if line.strip() != '':
-                            ofile.write(line.strip() + ' ')
-                ofile.write("\n\n")
-
-
-
-
 def get_parser():
     parser = argparse.ArgumentParser(description='Download and Prepare the Wikipedia')
     parser.add_argument('--mode', type=str,
