be43e866650d2da7248f689a6bf648c0cc2e6db1
barry-jin <barryjin1995@gmail.com>
2021-02-24 16:55:20 -0800

    update

--- ci/batch/submit-job.py
+++ ci/batch/submit-job.py
@@ -8,23 +8,60 @@ from datetime import datetime
 import boto3
 from botocore.compat import total_seconds
 
+instance_type_info = {
+    'g4dn.4x': {
+        'job_definition': 'gluon-nlp-g4dn_4xlarge:5',
+        'job_queue': 'g4dn'
+    },
+    'g4dn.8x': {
+        'job_definition': 'gluon-nlp-g4dn_8xlarge:5',
+        'job_queue': 'g4dn'
+    },
+    'g4dn.12x': {
+        'job_definition': 'gluon-nlp-g4dn_12xlarge:5',
+        'job_queue': 'g4dn-multi-gpu'
+    },
+    'p3.2x': {
+        'job_definition': 'gluon-nlp-p3_2xlarge:5',
+        'job_queue': 'p3'
+    },
+    'p3.8x': {
+        'job_definition': 'gluon-nlp-p3_8xlarge:5',
+        'job_queue': 'p3-4gpu'
+    },
+    'p3.16x': {
+        'job_definition': 'gluon-nlp-p3_16xlarge:5',
+        'job_queue': 'p3-8gpu'
+    },
+    'p3dn.24x': {
+        'job_definition': 'gluon-nlp-p3_24xlarge:5',
+        'job_queue': 'p3dn-8gpu'
+    },
+    'c5n.4x': {
+        'job_definition': 'gluon-nlp-c5_4xlarge:3',
+        'job_queue': 'c5n'
+    },
+    'c5n.18x': {
+        'job_definition': 'gluon-nlp-c5_18xlarge:3',
+        'job_queue': 'c5n'
+    }
+}
+
 parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
 parser.add_argument('--profile', help='profile name of aws account.', type=str,
                     default=None)
 parser.add_argument('--region', help='Default region when creating new connections', type=str,
-                    default=None)
+                    default='us-east-1')
 parser.add_argument('--name', help='name of the job', type=str, default='dummy')
-parser.add_argument('--job-queue', help='name of the job queue to submit this job', type=str,
-                    default='gluon-nlp-jobs')
-parser.add_argument('--job-definition', help='name of the job job definition', type=str,
-                    default='gluon-nlp-jobs:8')
+parser.add_argument('--job-type', help='type of job to submit.', type=str,
+                    choices=instance_type_info.keys(), default='g4dn.4x')
 parser.add_argument('--source-ref',
                     help='ref in GluonNLP main github. e.g. master, refs/pull/500/head',
                     type=str, default='master')
 parser.add_argument('--work-dir',
-                    help='working directory inside the repo. e.g. scripts/sentiment_analysis',
-                    type=str, default='scripts/bert')
+                    help='working directory inside the repo. e.g. scripts/preprocess',
+                    type=str, default='scripts/preprocess')
 parser.add_argument('--saved-output',
                     help='output to be saved, relative to working directory. '
                          'it can be either a single file or a directory',
@@ -32,9 +69,6 @@ parser.add_argument('--saved-output',
 parser.add_argument('--save-path',
                     help='s3 path where files are saved.',
                     type=str, default='batch/temp/{}'.format(datetime.now().isoformat()))
-parser.add_argument('--conda-env',
-                    help='conda environment preset to use.',
-                    type=str, default='gpu/py3')
 parser.add_argument('--command', help='command to run', type=str,
                     default='git rev-parse HEAD | tee stdout.log')
 parser.add_argument('--remote',
@@ -44,10 +78,12 @@ parser.add_argument('--wait', help='block wait until the job completes. '
                     'Non-zero exit code if job fails.', action='store_true')
 parser.add_argument('--timeout', help='job timeout in seconds', default=None, type=int)
 
+
 args = parser.parse_args()
 
 session = boto3.Session(profile_name=args.profile, region_name=args.region)
 batch, cloudwatch = [session.client(service_name=sn) for sn in ['batch', 'logs']]
+
 
 def printLogs(logGroupName, logStreamName, startTime):
     kwargs = {'logGroupName': logGroupName,
@@ -55,7 +91,7 @@ def printLogs(logGroupName, logStreamName, startTime):
               'startTime': startTime,
               'startFromHead': True}
 
-    lastTimestamp = 0
+    lastTimestamp = startTime - 1
     while True:
         logEvents = cloudwatch.get_log_events(**kwargs)
 
@@ -72,17 +108,6 @@ def printLogs(logGroupName, logStreamName, startTime):
     return lastTimestamp
 
 
-def getLogStream(logGroupName, jobName, jobId):
-    response = cloudwatch.describe_log_streams(
-        logGroupName=logGroupName,
-        logStreamNamePrefix=jobName + '/' + jobId
-    )
-    logStreams = response['logStreams']
-    if not logStreams:
-        return ''
-    else:
-        return logStreams[0]['logStreamName']
-
 def nowInMillis():
     endTime = long(total_seconds(datetime.utcnow() - datetime(1970, 1, 1))) * 1000
     return endTime
@@ -93,17 +118,17 @@ def main():
     logGroupName = '/aws/batch/job'
 
     jobName = re.sub('[^A-Za-z0-9_\-]', '', args.name)[:128]  # Enforce AWS Batch jobName rules
-    jobQueue = args.job_queue
-    jobDefinition = args.job_definition
+    jobType = args.job_type
+    jobQueue = instance_type_info[jobType]['job_queue']
+    jobDefinition = instance_type_info[jobType]['job_definition']
     command = args.command.split()
     wait = args.wait
 
-    parameters={
+    parameters = {
         'SOURCE_REF': args.source_ref,
         'WORK_DIR': args.work_dir,
         'SAVED_OUTPUT': args.saved_output,
         'SAVE_PATH': args.save_path,
-        'CONDA_ENV': args.conda_env,
         'COMMAND': args.command,
         'REMOTE': args.remote
     }
@@ -124,22 +149,23 @@ def main():
     running = False
     status_set = set()
     startTime = 0
-
+    logStreamName = None
     while wait:
         time.sleep(random.randint(5, 10))
         describeJobsResponse = batch.describe_jobs(jobs=[jobId])
         status = describeJobsResponse['jobs'][0]['status']
         if status == 'SUCCEEDED' or status == 'FAILED':
+            if logStreamName:
+                startTime = printLogs(logGroupName, logStreamName, startTime) + 1
             print('=' * 80)
             print('Job [{} - {}] {}'.format(jobName, jobId, status))
-
             sys.exit(status == 'FAILED')
 
         elif status == 'RUNNING':
-            logStreamName = getLogStream(logGroupName, jobName, jobId)
+            logStreamName = describeJobsResponse['jobs'][0]['container']['logStreamName']
             if not running:
                 running = True
-                print('\rJob [{} - {}] is RUNNING.'.format(jobName, jobId))
+                print('\rJob [{}, {}] is RUNNING.'.format(jobName, jobId))
                 if logStreamName:
                     print('Output [{}]:\n {}'.format(logStreamName, '=' * 80))
             if logStreamName:
@@ -149,6 +175,7 @@ def main():
             print('\rJob [%s - %s] is %-9s... %s' % (jobName, jobId, status, spin[spinner % len(spin)]),)
             sys.stdout.flush()
             spinner += 1
+
 
 if __name__ == '__main__':
     main()
