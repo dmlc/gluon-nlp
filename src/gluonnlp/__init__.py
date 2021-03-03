ed8253798c8198df88869c3cca43cfd5d7273afd
Xingjian Shi <xshiab@connect.ust.hk>
2021-03-03 10:38:05 -0800

    move mxnet to namespace

--- src/gluonnlp/__init__.py
+++ src/gluonnlp/__init__.py
@@ -1,14 +1,5 @@
 __version__ = '1.0.0.dev'
 from . import base
 from . import data
-from . import models
-from . import utils
-from . import attention_cell
-from . import initializer as init
-from . import layers
-from . import loss
-from . import lr_scheduler
-from . import op
-from . import torch
-from . import sequence_sampler
 from . import embedding
+from . import utils
