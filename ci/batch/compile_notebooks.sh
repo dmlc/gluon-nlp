e5392fd9bf354b81ca5a4a7e39dda4e51411da50
barry-jin <69359374+barry-jin@users.noreply.github.com>
2021-03-10 12:08:12 -0800

    [v0.x] Remove Notebook Compiling Steps (#1537)
    * remove notebook compiling logs
    
    * update compile_notebook.sh

--- ci/batch/compile_notebooks.sh
+++ ci/batch/compile_notebooks.sh
@@ -4,57 +4,18 @@
 event=$1
 ref=$2
 
-FAIL=0
-
-compile_notebook () {
-    local MDFILE=$1
-    DIR=$(dirname $MDFILE)
-    BASENAME=$(basename $MDFILE)
-    TARGETNAME=$(dirname $MDFILE)/${BASENAME%.md}.ipynb
-    LOGNAME=$(dirname $MDFILE)/${BASENAME%.md}.stdout.log
-
-    echo Compiling $BASENAME ...
-
-    python3 docs/md2ipynb.py ${MDFILE} &> $LOGNAME
-
-    EXIT_CODE=$?
-
-    if [ $EXIT_CODE -ne 0 ]; then
-        echo Compiling $BASENAME Failed, please download Notebook_Logs in build Artifacts for more details.
-    else
-        echo Compiling $BASENAME Succeeded
-    fi
+make docs
+EXIT_CODE=$?
+if [ $EXIT_CODE -ne 0 ]; then
+    echo Building Website Failed.
     exit $EXIT_CODE
-}
-
-pids=()
-
-for f in $(find docs/tutorials -type f -name '*.md' -print); do
-    compile_notebook "$f" &
-    pids+=($!)
-done;
-
-for pid in "${pids[@]}"; do
-    wait "$pid" || let "FAIL+=1"
-done;
-
-if [ "$FAIL" == "0" ]; then
-    echo Building Website
-    make docs
-    EXIT_CODE=$?
-    if [ $EXIT_CODE -ne 0 ]; then
-        echo Building Website Failed.
-        exit $EXIT_CODE
-    else
-        echo Building Website Succeeded.
-        if [ "$1" == "push" ]; then
-            echo "Uploading docs to s3://gluon-nlp/$2/"
-            aws s3 sync --delete ./docs/_build/html/ s3://gluon-nlp/$2/ --quiet --acl public-read
-        else
-            echo "Uploading docs to s3://gluon-nlp-staging/PR$1/$2/"
-            aws s3 sync --delete ./docs/_build/html/ s3://gluon-nlp-staging/PR$1/$2/ --quiet --acl public-read
-        fi
-    fi
 else
-    exit 1
+    echo Building Website Succeeded.
+    if [ "$1" == "push" ]; then
+        echo "Uploading docs to s3://gluon-nlp/$2/"
+        aws s3 sync --delete ./docs/_build/html/ s3://gluon-nlp/$2/ --quiet --acl public-read
+    else
+        echo "Uploading docs to s3://gluon-nlp-staging/PR$1/$2/"
+        aws s3 sync --delete ./docs/_build/html/ s3://gluon-nlp-staging/PR$1/$2/ --quiet --acl public-read
+    fi
 fi
