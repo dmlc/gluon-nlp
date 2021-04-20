a041b9af7f7e18a14f3ce68e083fb3982b403d38
barry-jin <barryjin1995@gmail.com>
2021-04-19 17:14:48 -0700

    udpate website build

--- ci/batch/compile_notebooks.sh
+++ ci/batch/compile_notebooks.sh
@@ -1,21 +1,54 @@
 #!/bin/bash
 # Shell script for submitting AWS Batch jobs to compile notebooks
-
 event=$1
 ref=$2
 
-make docs
-EXIT_CODE=$?
-if [ $EXIT_CODE -ne 0 ]; then
-    echo Building Website Failed.
-    exit $EXIT_CODE
-else
-    echo Building Website Succeeded.
-    if [ "$1" == "push" ]; then
-        echo "Uploading docs to s3://gluon-nlp/$2/"
-        aws s3 sync --delete ./docs/_build/html/ s3://gluon-nlp/$2/ --quiet --acl public-read
+FAIL=0
+
+compile_notebook () {
+    local MDFILE=$1
+    DIR=$(dirname $MDFILE)
+    BASENAME=$(basename $MDFILE)
+    TARGETNAME=$(dirname $MDFILE)/${BASENAME%.md}.ipynb
+    LOGNAME=$(dirname $MDFILE)/${BASENAME%.md}.stdout.log
+
+    echo Compiling $BASENAME ...
+
+    python3 docs/md2ipynb.py ${MDFILE} &> $LOGNAME
+
+    EXIT_CODE=$?
+
+    if [ $EXIT_CODE -ne 0 ]; then
+        echo Compiling $BASENAME Failed, please download Notebook_Logs in build Artifacts for more details.
+        exit $EXIT_CODE
     else
-        echo "Uploading docs to s3://gluon-nlp-staging/PR$1/$2/"
-        aws s3 sync --delete ./docs/_build/html/ s3://gluon-nlp-staging/PR$1/$2/ --quiet --acl public-read
+        echo Compiling $BASENAME Succeeded
     fi
+}
+
+for f in $(find docs/examples -type f -name '*.md' -print); do
+    compile_notebook "$f"
+done;
+
+EXIT_CODE=$?
+
+if [ $EXIT_CODE -eq 0 ]; then
+    echo Building Website
+    make docs
+    EXIT_CODE=$?
+    if [ $EXIT_CODE -ne 0 ]; then
+        echo Building Website Failed.
+        exit $EXIT_CODE
+    else
+        echo Building Website Succeeded.
+        if [ "$1" == "push" ]; then
+            echo "Uploading docs to s3://gluon-nlp/$2/"
+            aws s3 sync --delete ./docs/_build/html/ s3://gluon-nlp/$2/ --quiet --acl public-read
+        else
+            echo "Uploading docs to s3://gluon-nlp-staging/PR$1/$2/"
+            aws s3 sync --delete ./docs/_build/html/ s3://gluon-nlp-staging/PR$1/$2/ --quiet --acl public-read
+        fi
+    fi
+else
+    exit 1
 fi
