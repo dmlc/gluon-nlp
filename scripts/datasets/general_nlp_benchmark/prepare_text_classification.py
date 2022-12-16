ce0e0a21c23de76d349571e93e8f382caaf1f7b5
TrellixVulnTeam <112716341+TrellixVulnTeam@users.noreply.github.com>
2022-12-15 18:28:51 -0600

    Adding tarfile member sanitization to extractall() (#1593)

--- scripts/datasets/general_nlp_benchmark/prepare_text_classification.py
+++ scripts/datasets/general_nlp_benchmark/prepare_text_classification.py
@@ -53,7 +53,26 @@ def main(args):
         sha1_hash = _URL_FILE_STATS[file_url]
         download_path = download(file_url, args.cache_path, sha1_hash=sha1_hash)
         with tarfile.open(download_path) as f:
-            f.extractall(task_dir_path)
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(f, task_dir_path)
         if task == 'imdb':
             shutil.move(os.path.join(task_dir_path, 'imdb', 'train.parquet'),
                         os.path.join(task_dir_path, 'train.parquet'))
